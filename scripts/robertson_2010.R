59150bfcd6f96200e6653856563e3bfc3e814098
JoseBSL <barragansljose@gmail.com>
2018-10-10 15:28:05 +0200

    refs robertson

--- scripts/robertson_2010.R
+++ scripts/robertson_2010.R
@@ -7,7 +7,8 @@ d <- read.csv("data/data manipulation/robertson 2010/robertson_2010_4.csv", stri
 e <- read.csv("data/data manipulation/robertson 2010/robertson_2010_5.csv", stringsAsFactors = F)
 f <- read.csv("data/data manipulation/robertson 2010/robertson_2010_6.csv", stringsAsFactors = F)
 g <- read.csv("data/data manipulation/robertson 2010/robertson_2010_7.csv", stringsAsFactors = F)
-
+#REFERENCES
+h <- read.csv("data/data manipulation/robertson 2010/robertson_2010_references.csv", header=F)
 
 #trying for loop
 
@@ -19,6 +20,19 @@ colnames(i) <-  i[1, ]
 i = i[-1, ]
 y <- rbind(y, i)
 }
-
 colnames(y) <- colnames(a)
-rbind(a,y)
+robertson_2010 <- rbind(a,y)
+robertson_2010=robertson_2010[,-c(3,5)]
+
+#split references to take just year, author and journal
+colnames(h)[1] <- "references"
+h$references <- as.character(h$references)
+
+library(stringr)
+
+h <- str_split(h$references, " ", simplify = T)
+
+
+
+
+
