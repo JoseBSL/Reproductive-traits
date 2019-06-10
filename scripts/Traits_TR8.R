87424c671f175be89fff45cd7298a05b09f7027d
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2019-06-10 17:34:27 +1000

    LOADING traits with definitive list of spp, some of the traits are giving somo errors...

--- scripts/Traits_TR8.R
+++ scripts/Traits_TR8.R
@@ -40,27 +40,70 @@ write.csv(data_species,"data_species.csv")
 data_species=data_species[,-c(1,2,4,5,7,8)]
 write.csv(data_species,"taxsize_output.csv")
 
+
+spp <- read.csv("data/spp.csv")
 #First I'm going to try to download info just from biolflor
 #load my species list
-data_species=data_species[complete.cases(data_species),]
-data_list<- data_species$species
+spp$Species_all<-as.character(spp$Species_all)
+spp_list <- spp[,c(4)]
+spp_list<- as.data.frame(spp_list)
+spp_list[spp_list==""]<-NA
+spp_complete=spp_list[complete.cases(spp_list),]
+spp_complete<- as.data.frame(spp_complete)
+species <- spp_complete$spp_complete
+species <- as.character(species)
+str(species)
+
+#data_species=spp[complete.cases(spp),4]
+#data_species<-as.data.frame(data_species)
+#data_list<- data_species$species
 #Check what traits are available
-available_tr8
+#available_tr8
 
-b <- c("h_max", "h_min","li_form","reprod_meth","propag","growth_form","life_span","woodiness")
-            
-traits<-c("h_max", "h_min","li_form","reprod_meth","propag","growth_form","life_span","woodiness","li_form_B","li_span","reprod_B","strategy","self-st","Breeding_sys","apomixis","life_form_P","inflorescence_fr","poll_vect_fr",
+
+a <- c("h_max", "h_min","li_form")
+
+b <- c("reprod_meth","propag","growth_form","life_span","woodiness")
+
+c <- c("li_form_B","li_span","reprod_B","strategy","self-st","Breeding_sys")
+
+d <- c("apomixis")
+
+d_1 <- c("apomixis","life_form_P","inflorescence_fr","poll_vect_fr",
+       "fruit_type_fr","flower_colour_fr","li_form_fr")
+
+e <- c("apomixis","life_form_P","inflorescence_fr","poll_vect_fr",
+       "fruit_type_fr","flower_colour_fr","li_form_fr")
+
+f <- c("max_height_cal","Growth.Habit","Flower.Color",
+       "Growth.Form","Growth.Rate","Height.Mature","Lifespan",
+       "Bloom.Period","Vegetative.Spread.Rate")
+
+traits<-c("h_max", "h_min","li_form","reprod_meth","propag","growth_form","life_span","woodiness",
+          "li_form_B","li_span","reprod_B","strategy","self-st","Breeding_sys","apomixis","life_form_P","inflorescence_fr","poll_vect_fr",
      "fruit_type_fr","flower_colour_fr","li_form_fr","max_height_cal","Growth.Habit","Flower.Color",
-     "Growth.Form","Growth.Rate","Height..Mature","Lifespan","Bloom.Period","Vegetative.Spread.Rate")
+     "Growth.Form","Growth.Rate","Height.Mature","Lifespan","Bloom.Period","Vegetative.Spread.Rate")
 
-available_tr8
-retreved_traits<-tr8(species_list=data_list,download_list=b)
-retreved_traits<-tr8(species_list=data_list,download_list=traits)
+#available_tr8
+#retreved_traits<-tr8(species_list=data_list,download_list=b)
 
-life_form<-tr8(species_list=data_list,download_list="li_form")
-reprod_meth<-tr8(species_list=data_list,download_list="reprod_meth")
 
-save.image("evn.Rdata")
+retreved_traits<-tr8(species_list=species,download_list=a)
+retreved_traits_1<-tr8(species_list=species,download_list=b)
+retreved_traits_2<-tr8(species_list=species,download_list=c)
+retreved_traits_3_1<-tr8(species_list=species,download_list=d)
+retreved_traits_3_2<-tr8(species_list=species,download_list=d_1)
+
+#I haven't been able to run 3 gives an error
+retreved_traits_4<-tr8(species_list=species,download_list=e)
+#I haven't been able to run 4 either
+retreved_traits_5<-tr8(species_list=species,download_list=f)
+
+
+#life_form<-tr8(species_list=data_list,download_list="li_form")
+#reprod_meth<-tr8(species_list=data_list,download_list="reprod_meth")
+
+#save.image("evn.Rdata")
 #Way to proceed for a merge
-str(reprod_meth)
-df_reprod_meth <- as.data.frame(reprod_meth@results)
+#str(reprod_meth)
+#df_reprod_meth <- as.data.frame(reprod_meth@results)
