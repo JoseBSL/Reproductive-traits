cb659b93f2bb59c55112ea7485b888ec64ede035
JoseBSL <barragansljose@gmail.com>
2019-09-17 08:49:23 +1000

    reading multiple csvs

--- Scripts/Spp_network_id.R
+++ Scripts/Spp_network_id.R
@@ -12,16 +12,8 @@ d1$id <- rep("benadi_2014_1" ,length(d1))
 d_1 <-d$X
 d1=as.data.frame(d_1)
 d1$id <- rep("bartomeus_2008_bat1ca" ,length(d1))
+setwd("Data/Data_networks")
 
 
-temp = list.files(path="Data/Data_networks", pattern="*.csv")
-myfiles = lapply(temp, read.delim)
-
-
-temp = list.files(path="Data/Data_networks/", pattern="*.csv")
+temp = list.files( pattern="*.csv")
 for (i in 1:length(temp)) assign(temp[i], read.csv(temp[i]))
-
-# Get the files names
-files = list.files(path="Data/Data_networks",pattern="*.csv")
-# First apply read.csv, then rbind
-myfiles = do.call(rbind, lapply(files, function(x) read.csv(x, stringsAsFactors = FALSE)))
