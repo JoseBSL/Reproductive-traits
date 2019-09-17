f9304d602031a717a2730ea526431649b5684b27
JoseBSL <barragansljose@gmail.com>
2019-09-17 11:47:42 +1000

    renaming files without space

--- Scripts/Spp_network_id.R
+++ Scripts/Spp_network_id.R
@@ -1,19 +1,13 @@
 #Here I'm going to read all the networks, create a list of spp
 #and associate each spp to a network id
 
-
-#Prueba
-d <- read.csv("Data/Data_networks/Benadi_2014_1.csv")
-
-d_1 <-d$X
-d1=as.data.frame(d_1)
-d1$id <- rep("benadi_2014_1" ,length(d1))
-
-d_1 <-d$X
-d1=as.data.frame(d_1)
-d1$id <- rep("bartomeus_2008_bat1ca" ,length(d1))
+#To read all the csv's I setwd on the specific folder
+#Then
 setwd("Data/Data_networks")
-
-
-temp = list.files( pattern="*.csv")
+temp <- list.files(pattern="*.csv")
 for (i in 1:length(temp)) assign(temp[i], read.csv(temp[i]))
+
+
+
+
+
