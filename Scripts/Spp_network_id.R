1e7a909332e98fdf6ca3b25a5202d443715c1caf
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2019-09-17 18:46:30 +1000

    Now I have a list of unique species per network. I think I have to start checking sql for handling the database

--- Scripts/Spp_network_id.R
+++ Scripts/Spp_network_id.R
@@ -1,13 +1,27 @@
 #Here I'm going to read all the networks, create a list of spp
 #and associate each spp to a network id
 
+#load library
+
+library(dplyr)
+
+
 #To read all the csv's I setwd on the specific folder
 #Then
 setwd("Data/Data_networks")
 temp <- list.files(pattern="*.csv")
-for (i in 1:length(temp)) assign(temp[i], read.csv(temp[i]))
+my.list <- list(for (i in 1:length(temp)) assign(temp[i], read.csv(temp[i])))
+
+my_files <- list.files(pattern = "\\.csv$")
+my_data <- lapply(my_files, read.csv)
+
+data_id_list <- lapply(seq_along(my_data), 
+       function(x) cbind(my_data[[x]], unique.id=temp[i]))
+
+my_names = c("X", "unique.id")
+result = lapply(data_id_list, "[",  my_names)
+
+all_dat_id <- bind_rows(result, .id = "column_label")
 
 
-
-
-
+all_dat_id_unique <- unique(all_dat_id[c("X","unique.id")])
