577ab18dc76a07163729b7bd7d1f874092953371
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2019-10-08 00:37:13 +1100

    cleaning

---
+++ Scripts/Merge_coordinates_with_raits.R
@@ -0,0 +1,11 @@
+#Step 2
+#Read data generated in "Spp_network_id"
+
+#I'm going to merge here the trait data and the coordinates (project geonet Saunders et al 2019)
+
+#load data
+traits <- read.csv("data/Output_processing_data/all_dat_id_unique.csv",na="")
+coordinates_and_id <- read.csv("data/Process_data/coordinates_idref.csv")
+
+head(traits)
+head(coordinates_and_id)
---
+++ Scripts/README.md
@@ -0,0 +1,6 @@
+# Explanation of the scripts (README.md)
+
+# 1) Spp_network_id: scrip to merge trait data and unique species of the networks
+
+# 2) Traits_TR8: r package to find traits
+
