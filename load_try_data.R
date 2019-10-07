072f5c7531c18f273f426486f714f6b68a511034
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2019-10-07 21:11:00 +1100

    cleaning

--- load_try_data.R
+++
@@ -1,2 +0,0 @@
-require(data.table)
-TRYdata <- fread("reproductive_traits_try.txt", header = T, sep = "\t", dec = ".", quote = "", data.table = T)
--- project_folders.R
+++
@@ -1,8 +0,0 @@
-# Project organization
-
-#dir.create("scripts")
-#dir.create("data")
-#dir.create("data/data manipulation")
-#dir.create("data/data manipulation/robertson 2010")
-
-
