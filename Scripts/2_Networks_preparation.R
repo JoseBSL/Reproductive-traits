d02e330099132faf2e19900c5f06e46b7a43659c
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2020-02-05 19:32:23 +0100

    Metadata inoue 1990

--- Scripts/2_Networks_preparation.R
+++ Scripts/2_Networks_preparation.R
@@ -181,8 +181,8 @@ metadata_1 <- data.frame(csv_file_name, longitude, latitude, country, location,d
 
  
  #"Shelfanger, Norfolk", "Hickling Broad National Nature Reserve"
-dicks_1 <- read.csv("Data/Data_networks/dicks_2002_1.csv")
-dicks_2 <- read.csv("Data/Data_networks/dicks_2002_2.csv")
+dicks_1 <- read.csv("Data_networks/6_dicks_2002_1.csv")
+dicks_2 <- read.csv("Data_networks/6_dicks_2002_2.csv")
 
 dicks_1 <- melt(dicks_1)
 dicks_2 <- melt(dicks_2)
@@ -197,7 +197,7 @@ metaweb_dicks_2002$Pollinator_species=gsub("\\."," ",metaweb_dicks_2002$Pollinat
 metaweb_dicks_2002 <- acast(metaweb_dicks_2002, Plant_species ~ Pollinator_species , value.var='Interaction', 
                                              fun.aggregate=sum)
  
- write.csv(metaweb_dicks_2002, "Data_networks_metawebs/5_metaweb_dicks_2002.csv")
+ write.csv(metaweb_dicks_2002, "Data_networks_metawebs/6_metaweb_dicks_2002.csv")
  
  
  
@@ -248,13 +248,190 @@ National Nature Reserve")
  colnames(dupont_2) <- c("Plant_species", "Pollinator_species", "Interaction")
  
  
- metaweb_dupont_2002 <- rbind(dupont_1,dupont_2)
- metaweb_dupont_2002$Pollinator_species=gsub("\\."," ",metaweb_dupont_2002$Pollinator_species)
+ metaweb_dupont_2009 <- rbind(dupont_1,dupont_2)
+ metaweb_dupont_2009$Pollinator_species=gsub("\\."," ",metaweb_dupont_2009$Pollinator_species)
  
- metaweb_dupont_2002 <- acast(metaweb_dupont_2002, Plant_species ~ Pollinator_species , value.var='Interaction', 
+ metaweb_dupont_2009 <- acast(metaweb_dupont_2009, Plant_species ~ Pollinator_species , value.var='Interaction', 
                              fun.aggregate=sum)
  
- write.csv(metaweb_dupont_2002, "Data_networks_metawebs/7_metaweb_dupont_2002.csv")
+ write.csv(metaweb_dupont_2009, "Data_networks_metawebs/7_metaweb_dupont_2009.csv")
  
  
+ csv_file_name <- c("7_dupont_2009.csv")
  
+ longitude <- c("9.1; 9.266667")
+ 
+ latitude <- c("56.1; 56.066667")
+ 
+ country <- c("Denmark")
+ 
+ location <- c("Fields near Ikast; Isen bjerg")
+ 
+ duration <- c("1 season")
+ 
+ experiment_year <- c("2005")
+ 
+ unique_networks <-c(2)
+ 
+ plant_species <- c(31)
+ 
+ pollinator_species <- c(329)  
+ 
+ network_size <- c(10199)
+ 
+ metadata_5 <- data.frame(csv_file_name, longitude, latitude, country, location,duration, 
+                          experiment_year,unique_networks,plant_species, pollinator_species, 
+                          network_size)
+ 
+ metadata <- rbind(metadata, metadata_5)
+ 
+ 
+ #8th  Elberling 1999
+ 
+#Unique network
+ 
+ 
+ csv_file_name <- c("8_elberling_1999.csv")
+ 
+ longitude <- c(18.5)
+ 
+ latitude <- c(68.35)
+ 
+ country <- c("Sweden")
+ 
+ location <- c("Latnjajaure, Abisko")
+ 
+ duration <- c("1 season")
+ 
+ experiment_year <- c("1994")
+ 
+ unique_networks <-c(1)
+ 
+ plant_species <- c(24)
+ 
+ pollinator_species <- c(118)  
+ 
+ network_size <- c(2832)
+ 
+ metadata_6 <- data.frame(csv_file_name, longitude, latitude, country, location,duration, 
+                          experiment_year,unique_networks,plant_species, pollinator_species, 
+                          network_size)
+ 
+ metadata <- rbind(metadata, metadata_6)
+ 
+ 
+ #9th  Elberling unpublished data
+ 
+ 
+ csv_file_name <- c("9_elberling_unpublished_data.csv")
+ 
+ longitude <- c(-20.5)
+ 
+ latitude <- c(74.5)
+ 
+ country <- c("Greenland")
+ 
+ location <- c("Zackenberg")
+ 
+ duration <- c(NA)
+ 
+ experiment_year <- c(NA)
+ 
+ unique_networks <-c(NA)
+ 
+ plant_species <- c(31)
+ 
+ pollinator_species <- c(76)  
+ 
+ network_size <- c(2356)
+ 
+ metadata_7 <- data.frame(csv_file_name, longitude, latitude, country, location,duration, 
+                          experiment_year,unique_networks,plant_species, pollinator_species, 
+                          network_size)
+ 
+ metadata <- rbind(metadata, metadata_7)
+ 
+ 
+ #10th  Fang & Huang 2012
+ 
+ 
+fang_huang_1 <- read.csv("Data_networks/10_fang_huang_2008.csv")
+fang_huang_2 <- read.csv("Data_networks/10_fang_huang_2009.csv")
+fang_huang_3 <- read.csv("Data_networks/10_fang_huang_2010.csv")
+ 
+fang_huang_1 <- melt(fang_huang_1)
+fang_huang_2 <- melt(fang_huang_2)
+fang_huang_3 <- melt(fang_huang_3)
+ 
+ colnames(fang_huang_1) <- c("Plant_species", "Pollinator_species", "Interaction")
+ colnames(fang_huang_2) <- c("Plant_species", "Pollinator_species", "Interaction")
+ colnames(fang_huang_3) <- c("Plant_species", "Pollinator_species", "Interaction")
+ 
+ 
+ metaweb_fang_huang_2012 <- rbind(fang_huang_1,fang_huang_2,fang_huang_3)
+ metaweb_fang_huang_2012$Pollinator_species=gsub("\\."," ",metaweb_fang_huang_2012$Pollinator_species)
+ 
+ metaweb_fang_huang_2012 <- acast(metaweb_fang_huang_2012, Plant_species ~ Pollinator_species , value.var='Interaction', 
+                              fun.aggregate=sum)
+ 
+ write.csv(metaweb_fang_huang_2012, "Data_networks_metawebs/10_metaweb_fang_huang_2012.csv")
+ 
+
+  csv_file_name <- c("10_metaweb_fang_huang_2012.csv")
+ 
+ longitude <- c(99.63806)
+ 
+ latitude <- c(27.90139)
+ 
+ country <- c("China")
+ 
+ location <- c("Shangri-La Alpine Botanical Garden, Yunnan")
+ 
+ duration <- c("3 seasons")
+ 
+ experiment_year <- c("2008-2010")
+ 
+ unique_networks <-c(3)
+ 
+ plant_species <- c(130)
+ 
+ pollinator_species <- c(247)  
+ 
+ network_size <- c(32110)
+ 
+ metadata_8 <- data.frame(csv_file_name, longitude, latitude, country, location,duration, 
+                          experiment_year,unique_networks,plant_species, pollinator_species, 
+                          network_size)
+ 
+ metadata <- rbind(metadata, metadata_8)
+ 
+ 
+ #11th  Inoue et al., 1990
+ 
+csv_file_name <- c("11_metaweb_inoue_1990.csv")
+
+longitude <- c(99.63806)
+
+latitude <- c(27.90139)
+
+country <- c("China")
+
+location <- c("Shangri-La Alpine Botanical Garden, Yunnan")
+
+duration <- c("3 seasons")
+
+experiment_year <- c("2008-2010")
+
+unique_networks <-c(3)
+
+plant_species <- c(130)
+
+pollinator_species <- c(247)  
+
+network_size <- c(32110)
+
+metadata_8 <- data.frame(csv_file_name, longitude, latitude, country, location,duration, 
+                         experiment_year,unique_networks,plant_species, pollinator_species, 
+                         network_size)
+
+metadata <- rbind(metadata, metadata_8)
