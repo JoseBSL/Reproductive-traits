c180fe6d49667ef600489f93a25ab9f1ebb8d1d0
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2020-02-06 13:35:35 +0100

    Csv files ready just have to prepare the metaweb now

---
+++ Scripts/Processing_kaiser_bunbury_2009.R
@@ -0,0 +1,8 @@
+
+#Read Kaiser-bunbury 2009 Networks
+#DOI: https://doi.org/10.1016/j.ppees.2009.04.001
+#Prepare individual networks and metaweb
+
+restored_kaiser_bunbury_2009 <- read.csv("Data/Data_networks_processing/restored_kaiser_bunbury_2009.csv")
+control_kaiser_bunbury_2009 <- read.csv("Data/Data_networks_processing/control_kaiser_bunbury_2009.csv")
+
