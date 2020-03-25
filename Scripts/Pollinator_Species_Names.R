91baa6879b085ff38dbd6bf68dbaa6f9d687dcaf
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2020-03-25 11:38:51 +0100

    All pollinator species names corrected

--- Scripts/Pollinator_Species_Names.R
+++ Scripts/Pollinator_Species_Names.R
@@ -1,6 +1,5 @@
 # GROUP POLLINATOR TAXA AND CHECK NAMES
 
-
 # Load Libraries
 library(readxl)
 library(ggplot2)
@@ -16,12 +15,16 @@ Long_format_metawebs <- readRDS("Data/RData/Long_format_metawebs.RData")
 # SUBSET UNIQUE CASES PER NETWORK
 Long_format_subset_poll <-  unique(Long_format_metawebs[,c("Pollinator_species","Id")])
 
-# SELECT SUBSET
+# SELECT FIRST WORD OF THE SPECIES (GENUS), AND UNIQUE CASE
 poll <- as.data.frame(unique(word(Long_format_subset_poll$Pollinator_species),1))
 str(poll)
 
 colnames(poll)[1] <- "Genus"
 
+#RUN TAXSIZE
+#I HAVE TO SPLIT THE DATAFRAME BECAUSE THE PACKAGE IS GIVING A LOT OF ERRORS WITH BIG DATASETS (FOR ME AT LEAST)
+
+#I START WITH 50 SPP
 poll_50 <- poll[c(1:50),]
 poll_50 <- as.data.frame(poll_50)
 colnames(poll_50)[1] <- "Genus"
@@ -52,9 +55,61 @@ colnames(poll_200)[1] <- "Genus"
 poll_species_200=tax_name(query=poll_200$Genus,get=c("genus", "order", "family"),db="itis",division_filter = "Arthropoda",rank_query="genus")
 poll_200 <- poll_species_200
 
-poll_250 <- poll[c(201:250),]
-poll_250 <- as.data.frame(poll_250)
-colnames(poll_250)[1] <- "Genus"
+poll_species_200 <- rbind(poll_50,poll_100,poll_150, poll_200)
+saveRDS(poll_species_200, "Data/RData/poll_species_200.RData")
+write.csv(poll_species_200, "Data/Data_processing/poll_200.csv")
 
-poll_species_250=tax_name(query=poll_250$Genus,get=c("genus", "order", "family"),db="itis",division_filter = "Arthropoda",rank_query="genus")
-poll_250 <- poll_species_250
+poll_400 <- poll[c(201:400),]
+poll_400 <- as.data.frame(poll_400)
+colnames(poll_400)[1] <- "Genus"
+
+poll_species_400=tax_name(query=poll_400$Genus,get=c("genus", "order", "family"),db="itis",division_filter = "Arthropoda",rank_query="genus")
+poll_400 <- poll_species_400
+write.csv(poll_species_400, "Data/Data_processing/poll_400.csv")
+
+
+poll_800<- poll[c(401:800),]
+poll_800 <- as.data.frame(poll_800)
+colnames(poll_800)[1] <- "Genus"
+
+poll_species_800=tax_name(query=poll800$Genus,get=c("genus", "order", "family"),db="itis",division_filter = "Arthropoda",rank_query="genus")
+poll_800 <- poll_species_800
+write.csv(poll_species_800, "Data/Data_processing/poll_800.csv")
+
+poll_1400<- poll[c(801:1400),]
+poll_1400 <- as.data.frame(poll_1400)
+colnames(poll_1400)[1] <- "Genus"
+
+poll_species_1400=tax_name(query=poll1400$Genus,get=c("genus", "order", "family"),db="itis",division_filter = "Arthropoda",rank_query="genus")
+poll_1400 <- poll_species_1400
+write.csv(poll_species_1400, "Data/Data_processing/poll_1400.csv")
+
+poll_2065<- poll[c(1401:2065),]
+poll_2065 <- as.data.frame(poll_2065)
+colnames(poll_2065)[1] <- "Genus"
+
+poll_species_2065=tax_name(query=poll2065$Genus,get=c("genus", "order", "family"),db="itis",division_filter = "Arthropoda",rank_query="genus")
+poll_2065 <- poll_species_2065
+write.csv(poll_species_2065, "Data/Data_processing/poll_2065.csv")
+
+
+#NOW I HAVE ALL THE SPECIES
+#BIND DATASETS
+
+poll_200 <- read.csv("Data/Data_processing/poll_200.csv")
+poll_400 <- read.csv("Data/Data_processing/poll_400.csv")
+poll_800 <- read.csv("Data/Data_processing/poll_800.csv")
+poll_1400 <- read.csv("Data/Data_processing/poll_1400.csv")
+poll_2065 <- read.csv("Data/Data_processing/poll_2065.csv")
+
+poll_spp <- rbind(poll_200, poll_400, poll_800, poll_1400, poll_2065)
+poll_spp$genus_old <- poll$Genus
+poll_spp <- poll_spp[,-1]
+write.csv(poll_spp, "Data/Data_processing/poll_spp.csv")
+
+
+
+
+
+
+
