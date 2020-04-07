14add01f54972418b838aa335e172093abc1973c
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2020-04-07 17:39:58 +0200

    Updating long format networks with quantitative data from some networks

--- Scripts/Spp_richness_plot.R
+++ Scripts/Spp_richness_plot.R
@@ -38,13 +38,13 @@ data_wide <- spread(data_long, Id, measurement)
 #Select colours from a Palette that Nacho made for http://www.pnas.org/content/early/2015/11/24/1517092112.long
 clrs <- c("darkblue", "blue", "yellow", "cornsilk", "goldenrod",
           "orange")
-data2 <- data_wide[,c(2:29)]
+data2 <- data_wide[,c(2:31)]
 data2 <- data2[,order(data2[1,], decreasing=T)]
 data2 <- as.matrix(data2)
-colnames(data2) <- c("Ramirez 1989", "Ramirez 1992", "Bartomeus unp. 2015", "Chacoff 2011","Bartomeus 2008", "Bartomeus 2008", "Robertson 1929", "Small 1976",
-                     "Inouye 1990", "Souza 2018", "Bundgaard 2003", "Inouye 1988", "Dupont 2009", "Fang huang 2012", "mcmullen 1993", "Trivellone Unp.", "Dicks 2002",
-                     "Elberling 1999", "Primack 1983", "Primack 1983", "Kato 2000", "Kaiser-Bunbunry 2009", "Traveset 2013", "Kevan 1970", "Bek 2006",
-                     "Primack 1983", "Kaiser-Bunbury 2014", "Lundgren 2005")
+colnames(data2) <- c("Ramirez 1989", "Ramirez 1992", "Bartomeus unp. 2015", "Chacoff 2011","Bartomeus 2008 1", "Bartomeus 2008 2", "Robertson 1929", "Small 1976",
+                     "Inouye 1990", "Souza 2018", "Bundgaard 2003", "Inouye 1988", "Dupont 2009 1", "Olesen 2002 1", "Fang huang 2012", "mcmullen 1993", "Olesen 2002 2", "Dicks 2002",
+                     "Elberling 1999", "Primack 1983 1", "Primack 1983 2", "Kato 2000", "Kaiser-Bunbunry 2009", "Traveset 2013", "Kevan 1970", "Bek 2006",
+                     "Primack 1983 3", "Kaiser-Bunbury 2014", "Dupont 2009 2", "Lundgren 2005")
 
 saveRDS(data2, "Data/RData/Spp_richness_by_order.RData")
 
@@ -94,5 +94,32 @@ net_sum_subset$total_col <- rowSums(net_sum_subset[,-1])
 
 #Calculate percentage of visits from main orders
 
-data_network_main_orders[,c(2:6)] <- sapply(data_network_main_orders[,c(2:6)], function(x) {x /data_network_main_orders$total_col*100})
+net_sum_subset[,c(2:6)] <- sapply(net_sum_subset[,c(2:6)], function(x) {x /net_sum_subset$total_col*100})
 
+#Convert to long format for plotting
+net_long <- gather(net_sum_subset, orders, measurement, Hymenoptera:Hemiptera, factor_key=TRUE)
+
+#Convert again to wideformat for plotting with barplot/for ggplot the last format is enough
+net_long <- net_long[,c(-2)]
+net_wide <- spread(net_long, Id, measurement)
+net_wide <- net_wide[,c(2:18)]
+net_wide <- net_wide[,order(net_wide[1,], decreasing=T)]
+net_wide2 <- as.matrix(net_wide)
+
+#Select colours from a Palette that Nacho made for http://www.pnas.org/content/early/2015/11/24/1517092112.long
+clrs <- c("darkblue", "blue", "yellow", "cornsilk", "goldenrod",
+          "orange")
+
+colnames(data2) <- c("Ramirez 1989", "Ramirez 1992", "Bartomeus unp. 2015", "Chacoff 2011","Bartomeus 2008 1", "Bartomeus 2008 2", "Robertson 1929", "Small 1976",
+                     "Inouye 1990", "Souza 2018", "Bundgaard 2003", "Inouye 1988", "Dupont 2009 1", "Olesen 2002 1", "Fang huang 2012", "mcmullen 1993", "Olesen 2002 2", "Dicks 2002",
+                     "Elberling 1999", "Primack 1983 1", "Primack 1983 2", "Kato 2000", "Kaiser-Bunbunry 2009", "Traveset 2013", "Kevan 1970", "Bek 2006",
+                     "Primack 1983 3", "Kaiser-Bunbury 2014", "Dupont 2009 2", "Lundgren 2005")
+par(mar = c(16,6.1,4.1,8))
+barplot(net_wide2, beside = FALSE, ylab = "% visits", cex.names = 0.5
+        ,las = 2, col = clrs)
+abline (h = 0)
+par(xpd=TRUE)
+leg <- c("Hymenoptera", "Coleoptera", "Lepidoptera", "Diptera", "Hemiptera")
+legend(x = 35, y = 100, legend = rev(leg), cex = 0.4, fill = rev(clrs))
+par(mar = c(5,1.1,4.1,2.1))
+par(xpd=FALSE)
