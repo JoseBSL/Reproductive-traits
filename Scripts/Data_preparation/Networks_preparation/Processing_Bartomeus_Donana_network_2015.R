3cfb829478bbf2cda059a03ed729aa34b6051942
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2020-04-21 13:04:42 +0200

    saving all individual networks of Bee fun

--- Scripts/Data_preparation/Networks_preparation/Processing_Bartomeus_Donana_network_2015.R
+++ Scripts/Data_preparation/Networks_preparation/Processing_Bartomeus_Donana_network_2015.R
@@ -1,17 +1,35 @@
-
+#16 different networks
+#I just select transects
 
 #If this does not work, load data manually
 install.packages("devtools")
 require(devtools)
+library(devtools)
 install_github("BeeFunData", "ibartomeus")
 library(BeeFunData)
 library(reshape2)
 
-#Filter for the same methodology
-all_interactions <- subset(all_interactions, Out="transect")
-#Create matrix
-matrix <- acast(all_interactions, Plant_gen_sp ~ Pollinator_gen_sp , value.var='Frequency', 
-      fun.aggregate=sum, margins=TRUE)
-#Save csv and export with the other networks
+#Seems that is not working (do not know why)
+#I load data manually
 
-write.csv(matrix, "data/matrix/Data_networks_subset/bartomeus_Donana.csv")
+
+#Filter for the same methodology
+all_interactions <- subset(all_interactions, Out=="transect")
+
+#check unique levels
+levels(unique(all_interactions$Site_ID))
+
+#Create a list with all the dataframes by Id
+my_list <- split(all_interactions , f = all_interactions$Site_ID )
+
+#Convert each dataframe to a matrix
+
+my_list_matrices <- lapply(my_list, function(x) acast(x, Plant_gen_sp ~ Pollinator_gen_sp , value.var='Frequency', 
+                                         fun.aggregate=sum, margins=F) )
+
+#Save each matrix individually
+for(i in names(my_list_matrices)){
+  write.csv(my_list_matrices[[i]], paste0("Data/Data_processing/Bartomeus_BeeFun/29_Bartomeus_", i,"_unp.csv"))
+}
+
+
--- Scripts/Traits_pollinator_taxa_plot.R
+++ Scripts/Traits_pollinator_taxa_plot.R
@@ -56,36 +56,6 @@ all[] <- lapply(all, make.true.NA)
 #3)SUBSET BY REAL INTERACTION (GREATER THAN 1) 
 #-----------------------------------------------------
 
-#SELECT INTERACTION THAT OCCURED
-#Subset by real interactions
-i <- subset(all, Interaction>0)
-#Now we have the number of poll. species recorded for each plat species 
-
-#-----------------------------------------------------
-#4)SELECT SELF, SELF IN AND PARTIALLY FOR NOW
-#-----------------------------------------------------
-
-i_s <- subset(i, Compatibility=="self_compatible"|Compatibility=="self_incompatible"|Compatibility=="partially_self_incompatible")
-
-
-#-----------------------------------------------------
-#5)SUBSET BY MAIN POLLINATOR ORDERS
-#-----------------------------------------------------
-str(i_s$Pollinator_order)
-i_s$Pollinator_order <- as.character(i_s$Pollinator_order)
-
-
-i_s_t <- subset(i_s, Pollinator_order=="Hymenoptera"|Pollinator_order=="Lepidoptera"|Pollinator_order=="Coleoptera"|Pollinator_order=="Diptera")
-
-
-#-----------------------------------------------------
-#6)PLOT IT
-#-----------------------------------------------------
-
-ggplot(i_s_t) +
-  geom_bar(aes(x = Compatibility, fill = Pollinator_order)) +
-  facet_wrap(~ Id) 
-
 
 #Maybe I have tu subset by pollinator too (4 main orders)
 
@@ -150,12 +120,7 @@ plotweb(trial, method="cca", text.rot="90",
         labsize=0.9,col.low="green4", col.high=c("gold","blue","red"),
         col.interaction="skyblue3", bor.col.interaction ="black" )
 
-#
-
-
-
-
-# CODE FROM: https://www.data-to-viz.com/graph/sankey.html
+#Sankey diagram
 
 # Libraries
 library(tidyverse)
@@ -164,37 +129,8 @@ library(patchwork)
 library(hrbrthemes)
 library(circlize)
 
-# Load dataset from github
-data <- read.table("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/13_AdjacencyDirectedWeighted.csv", header=TRUE)
-# Package
-library(networkD3)
 
-# I need a long format
-data_long <- data %>%
-  rownames_to_column %>%
-  gather(key = 'key', value = 'value', -rowname) %>%
-  filter(value > 0)
-colnames(data_long) <- c("source", "target", "value")
-data_long$target <- paste(data_long$target, " ", sep="")
-
-# From these flows we need to create a node data frame: it lists every entities involved in the flow
-nodes <- data.frame(name=c(as.character(data_long$source), as.character(data_long$target)) %>% unique())
-
-# With networkD3, connection must be provided using id, not using real name like in the links dataframe.. So we need to reformat it.
-data_long$IDsource=match(data_long$source, nodes$name)-1 
-data_long$IDtarget=match(data_long$target, nodes$name)-1
-
-# prepare colour scale
-ColourScal ='d3.scaleOrdinal() .range(["#FDE725FF","#B4DE2CFF","#6DCD59FF","#35B779FF","#1F9E89FF","#26828EFF","#31688EFF","#3E4A89FF","#482878FF","#440154FF"])'
-
-# Make the Network
-sankeyNetwork(Links = data_long, Nodes = nodes,
-              Source = "IDsource", Target = "IDtarget",
-              Value = "value", NodeID = "name", 
-              sinksRight=FALSE, colourScale=ColourScal, nodeWidth=40, fontSize=13, nodePadding=20)
-
-
-#COPY CODE OF THE MATRIX, THIS COULD SOLVE MY PROBLEM
+#subset for one network
 t <- subset(all, Id=="1_metaweb_carpobrotus_Bartomeus_2008.csv")
 t_sub <- t[,c(6,4,26)]
 #Remove NA's
@@ -209,7 +145,7 @@ matrix <- acast(t_sub, Pollinator_order ~ Compatibility , value.var='Interaction
 matrix_d <- as.data.frame(matrix)
 
 
-    # I need a long format
+# I need a long format
 data_long <- matrix_d %>%
   rownames_to_column %>%
   gather(key = 'key', value = 'value', -rowname) %>%
@@ -236,46 +172,6 @@ sankeyNetwork(Links = data_long, Nodes = nodes,
 
 
 
-
-str(data_long)
-
-
-
-
-
-
-#subset 1 network 
-t <- subset(all, Id=="1_metaweb_carpobrotus_Bartomeus_2008.csv")
-t_sub <- t[,c(4,3,26)]
-str(t_sub)
-#Remove NA's CHECK
-t_sub <- t_sub[complete.cases(t_sub), ]
-
-#Select values of intreraction greater than 0
-#t_sub <- t_sub[t_sub$Interaction>0,]
-
-#levels data frame 
-nodes <- data.frame("name"=unique(c(levels(as.factor(t_sub$Pollinator_species)),levels(as.factor(t_sub$Compatibility)))))
-str(t_sub)
-
-#Try to make the frequency column for each set of nodes
-#Fisr pollinators
-library(plyr)
-IDsource <- count(t_sub, 'Pollinator_species')
-IDtarget <- count(t_sub, 'Compatibility')
-
-
-y_sub <- merge(t_sub, IDsource,by= "Pollinator_species")
-colnames(y_sub)[4] <- "IDsource"
-
-final <- merge(y_sub, IDtarget,by= "Compatibility")
-colnames(final)[5] <- "IDtarget"
-str(final)
-
-sankeyNetwork(Links = final, Nodes = nodes,
-              Source = "IDsource", Target = "IDtarget",
-              Value = "Interaction", NodeID = "name", 
-              sinksRight=FALSE,  nodeWidth=40, fontSize=13, nodePadding=20)
 
 
 
