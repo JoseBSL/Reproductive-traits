a11d4b3e30c81dca7b2cdd4f5331a80dd944ac2a
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2020-04-22 21:14:53 +0200

    On the way of plotting the Sankey diagram for the 16 networks, not far!

--- Scripts/Preliminary_Analysis.R
+++ Scripts/Preliminary_Analysis.R
@@ -9,7 +9,10 @@ library(patchwork)
 library(hrbrthemes)
 library(circlize)
 library(reshape2)
-
+library(stringr)
+library(networkD3)
+library(magrittr)
+library(pagedown)
 
 #First read all networks
 #Set working directory to read files
@@ -73,13 +76,11 @@ poll$genus_old <- as.character(poll$genus_old)
 #Now merge with bart dataframe
 #But before I have to split the poll species names in two
 
-bart_1$genus <- word(bart_1$Pollinator_species)
-colnames(bart_1)[5] <- "genus_old"
-str(bart_1)
-head(bart_1)
+bart$genus_old <- word(bart$Pollinator_species)
+
 
 #Now merge by genus
-b <- merge(bart_1, poll, by="genus_old", all = F)
+b <- merge(bart, poll, by="genus_old", all = F)
 #data ready
 
 ###############################
@@ -89,17 +90,30 @@ b <- merge(bart_1, poll, by="genus_old", all = F)
 ###############################
 
 #select columns of interest
-b <- b[,c(2,4,5,8)]
+b_1 <- b[,c(3,4,5,23,78,79)]
 #Remove NA's
 #select complete cases
-b_1 <- b[complete.cases(b), ]
+b_1 <- b_1[complete.cases(b_1), ]
+b_1$order <- as.character(b_1$order)
+b_2 <- subset(b_1, order=="Hymenoptera"| order=="Diptera"| order=="Lepidoptera"| order=="Coleoptera")
+#Remove .csv from id
+b_2$Net_ID <- gsub("\\..*","",b_2$Net_ID)
 
+list_bee_fun <- split(b_2, b_2$Net_ID)
+#saveRDS(list_bee_fun, "Data/RData/list_bee_fun.RData")
+#list_bee_fun <- readRDS("Data/RData/list_bee_fun.RData")
 #Now find a way to convert to matrix and plot all
 
+for(i in names(list_bee_fun)){
+
+
 #convert to matrix
-matrix <- acast(t_sub, Pollinator_order ~ Compatibility , value.var='Interaction', 
+i <- acast(list_bee_fun[[i]], order ~ Compatibility , value.var='Interaction', 
                 fun.aggregate=sum, margins=F)
 
+}
+
+#TRY WITH A SAPPLY PROBABLY I CAN AVOID THE FOOR LOOP AND KEEP THE LIST STRUCTURE
 
 matrix_d <- as.data.frame(matrix)
 
@@ -112,6 +126,7 @@ data_long <- matrix_d %>%
 colnames(data_long) <- c("source", "target", "value")
 data_long$target <- paste(data_long$target, " ", sep="")
 
+
 # From these flows we need to create a node data frame: it lists every entities involved in the flow
 nodes <- data.frame(name=c(as.character(data_long$source), as.character(data_long$target)) %>% unique())
 
@@ -122,14 +137,16 @@ data_long$IDtarget=match(data_long$target, nodes$name)-1
 # prepare colour scale
 ColourScal ='d3.scaleOrdinal() .range(["#FDE725FF","#B4DE2CFF","#6DCD59FF","#35B779FF","#1F9E89FF","#26828EFF","#31688EFF","#3E4A89FF","#482878FF","#440154FF"])'
 
+setwd("~/R_Projects/Reproductive traits/Images/Sankey") 
+
 # Make the Network
 sankeyNetwork(Links = data_long, Nodes = nodes,
               Source = "IDsource", Target = "IDtarget",
               Value = "value", NodeID = "name", 
-              sinksRight=FALSE, colourScale=ColourScal, nodeWidth=40, fontSize=13, nodePadding=20)
+              sinksRight=FALSE, colourScale=ColourScal, nodeWidth=40, fontSize=13, nodePadding=20)%>%
+saveNetwork(list_bee_fun[[i]], file = paste0(i, ".html")) #ADD SPECIFIC FOLDER
 
+}
 
-
-
-
-
+write.csv(my_list_matrices[[i]], paste0("Data/Data_processing/Bartomeus_BeeFun/29_Bartomeus_", i,"_unp.csv"))
+}#FINISH LOOP 
