91464fc634a80283fe1299afe5b1ed09babe0c56
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.home>
2020-08-18 14:37:38 +0200

    Removing Na's from networks that are giving issues

--- Scripts/Data_preparation/Networks_preparation/Processing_Bartomeus_Donana_network_2015.R
+++ Scripts/Data_preparation/Networks_preparation/Processing_Bartomeus_Donana_network_2015.R
@@ -27,9 +27,22 @@ my_list <- split(all_interactions , f = all_interactions$Site_ID )
 my_list_matrices <- lapply(my_list, function(x) acast(x, Plant_gen_sp ~ Pollinator_gen_sp , value.var='Frequency', 
                                          fun.aggregate=sum, margins=F) )
 
+
+#convert NA'S to zeros
+my_list_matrices_1 <- rapply( my_list_matrices, f=function(x) ifelse(is.na(x),0,x), how="replace" )
+
+
+#select row sums different of zero
+my_list_matrices_2 <- lapply(my_list_matrices_1, function(x) (x[rowSums(x[, -1] > 0) != 0, ]))
+
+#select colsums different of zero
+my_list_matrices_3 <- lapply(my_list_matrices_2, function(x) (x[, colSums(x != 0) > 0]))
+
+
+setwd("~/R_Projects/Reproductive traits") 
 #Save each matrix individually
-for(i in names(my_list_matrices)){
-  write.csv(my_list_matrices[[i]], paste0("Data/Data_processing/bartomeus_beefun/29_Bartomeus_", i,"_unp.csv"))
+for(i in names(my_list_matrices_3)){
+  write.csv(my_list_matrices_3[[i]], paste0("Data/Data_processing/bartomeus_donana_unpublished/29_Bartomeus_", i,"_unp.csv"))
 }
 
 
