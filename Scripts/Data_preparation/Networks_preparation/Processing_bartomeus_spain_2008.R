3fd58d5e1fab2485043e48602bf41f467ca110a9
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.home>
2020-08-19 13:20:31 +0200

    processing species names

---
+++ Scripts/Data_preparation/Networks_preparation/Processing_bartomeus_spain_2008.R
@@ -0,0 +1,9 @@
+# Processing data from Bartomeus 2008
+# DOI: https://doi.org/10.1007/s00442-007-0946-1
+# Catalonia, Spain
+# IMPORTANT NOTE: THE SITES WERE SAMPLED in 2005
+# I'm going to prepare here the unique networks per site and year and the metaweb
+
+
+
+d <- read.csv("Data/Data_processing/bartomeus_spain_2008/Kaiser_bunbury_mauritius_2010.csv", header=T, stringsAsFactors=F)
