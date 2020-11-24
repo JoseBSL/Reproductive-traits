33de1bb3c7c24e98a7834566d7a8e66f984a3945
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.home>
2020-11-24 18:20:54 +0100

    Analysis z-scores

--- Scripts/6_Analysis_Z_scores_Guilds_Functional_Groups.R
+++ Scripts/6_Analysis_Z_scores_Guilds_Functional_Groups.R
@@ -13,8 +13,8 @@
 library(ape) #for phylogenetic distance
 library(dplyr) #data processing
 library(rtrees) #for phylogenetic distance
-library(phyr) #Analysis
 library(DHARMa)
+library(brms)
 
 ########################################################################################################################################################
 #1) READ DATA
@@ -30,6 +30,12 @@ d <- read.csv("Data/Csv/quantitative_networks_Z_scores_with_traits_and_clusters.
 d$Family_all <- as.character(d$Family_all)
 d$Genus_all <- as.character(d$Genus_all)
 d$Species_all <- as.character(d$Species_all)
+
+#These species are not present for analysis in the matrix, remove or model cannot have phylo as covariable
+d$Species_all[d$Species_all=="Diospyros seychellarum"] <- NA
+d$Species_all[d$Species_all=="Memecylon eleagni"] <- NA
+d$Species_all[d$Species_all=="Ocotea laevigata"] <- NA
+d$Species_all[d$Species_all=="Soulamea terminaloides"] <- NA
 
 #Make these NA's as NA
 d$Species_all[d$Species_all=="NA"] <- NA
@@ -66,24 +72,38 @@ rownames(A) <- gsub("_", " ", rownames(A))
 #Add phylo column to dataset
 d_1$phylo
 d_1$phylo <- d_1$Species_all
+str(d_1)
+
+d_1$Clusters <- as.character(d_1$Clusters)
+d_1$Clusters[d_1$Clusters=="1"] <- "A"
+d_1$Clusters[d_1$Clusters=="2"] <- "B"
+d_1$Clusters[d_1$Clusters=="3"] <- "C"
+d_1$Clusters[d_1$Clusters=="4"] <- "D"
+d_1$Clusters[d_1$Clusters=="5"] <- "E"
+
+d_1$Clusters <- as.factor(d_1$Clusters)
+
+
 
 ########################################################################################################################################################
-#1) PHYLOGENETIC DISTANCE OF THE SPECIES
+#3) Analysis
 ########################################################################################################################################################
-library("INLA")
-library("phyr")
-m1 <- pglmm(Z_scores ~ guild + (1 | phylo) +(1 | Id),
-            data = d_1,cov_ranef = list(sp = A),bayes = T, prior = "pc.prior.auto")
+m1 <- brm(Z_scores ~ guild*Clusters + (1|Id) + (1|gr(phylo, cov = A)),
+          data = d_1, family  = gaussian(),data2 = list(A = A), cores = 4,
+          sample_prior = TRUE, warmup = 500, iter = 1500,save_all_pars=T,
+          control = list(adapt_delta = 0.99)) 
+
 
 summary(m1)
-plot_bayes(m1, sort = TRUE)
-
-resids <- DHARMa::simulateResiduals(m1, plot = FALSE)
+library(ggplot2)
+pp_check(m1) +xlim(-10,10)+ylim(0,3)
+c_e <- conditional_effects(m1) 
+p1 <- plot(c_e, points=T,plot = FALSE)[[1]]
+bayes_R2(m1)
 
 ########################################################################################################################################################
 ########################################################################################################################################################
 ########################################################################################################################################################
 
-install.packages("remotes")
-remotes::install_github("daijiang/phyr")
+devtools::install_github("daijiang/phyr", force=T)
 
