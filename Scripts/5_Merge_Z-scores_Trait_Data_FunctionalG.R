05486c5749d349ce6fea97f58e6d1874d78a1848
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.home>
2020-11-26 19:02:35 +0100

    m

---
+++ Scripts/5_Merge_Z-scores_Trait_Data_FunctionalG.R
@@ -0,0 +1,80 @@
+########################################################################################################################################################
+#SCRIPT TO MERGE LONG DATA WITH Z-SCORES, TRAIT DATA AND FUNCTIONAL GROUPS
+
+#1) LOAD DATA 
+
+#3) MERGE WITH TRAIT DATA
+
+#4) MERGE WITH FUNCTIONAL GROUPS
+
+#5) SAVE DATA
+########################################################################################################################################################
+
+#LOAD LIBRARIES
+library(data.table)
+library(readxl) #read trait data
+
+########################################################################################################################################################
+#1) LOAD LONG FORMAT DATA WITH Z-SCORES, TRAIT DATA AND FUNCTIONAL GROUP DATA
+########################################################################################################################################################
+long_d_2 <- read.csv("Data/Csv/long_format_quantitative_networks_Z_scores.csv")
+t_data <- read_excel("Data/Trait_data_raw/Trait_data_final.xlsx")
+hclust_d_5 <- read.csv("Data/Csv/imputed_trait_data_hclust_5_clusters.csv") #5 clusters
+########################################################################################################################################################
+#3) MERGE WITH TRAIT DATA
+########################################################################################################################################################
+
+#process trait data
+t_data <- t_data[1:1701,]
+#filter data, select species with flower level info and capitulum
+t_data_filtered <- filter(t_data, Info_level == "flower" |  Info_level == "capitulum")
+t_data_filtered <- as.data.frame(t_data_filtered)
+colnames(t_data_filtered)[1] <- "Plant_species"
+#select columns of interest
+traits <- t_data_filtered %>% select(Species_geonet,Order_all,Family_all,Genus_all,Species_all,Breeding_system,IMPUTED_Compatibility,Autonomous_selfing_level,Autonomous_selfing_level_data_type,Autonomous_selfing_level_fruit_set,Flower_morphology,Flower_symmetry,Flowers_per_plant,Flowers_per_inflorescence,Floral_unit_width,Corolla_diameter_mean,Corolla_length_mean,STYLE_IMPUTED,OVULES_IMPUTED,life_form,lifespan,IMPUTED_plant_height_mean_m)
+#Remove duplicated species
+t <- traits[!duplicated(traits$Species_all), ]
+colnames(t)[1] <- "Plant_species"
+
+#MERGE NETWORK AND TRAIT DATA
+long_format_trait_data <- merge(long_d_2, t, by = "Plant_species", all.x =T)
+
+########################################################################################################################################################
+#4) MERGE WITH FUNCTIONAL GROUPS
+########################################################################################################################################################
+
+#5 CLUSTERS
+#Some data processing before merging
+#colnames to merge by same id
+colnames(hclust_d_5)[1] <-"Species_all"
+#convert to chracters
+hclust_d_5$Species_all <- as.character(hclust_d_5$Species_all)
+long_format_trait_data$Species_all <- as.character(long_format_trait_data$Species_all)
+
+#convert character NA's to NA's
+long_format_trait_data$Species_all[long_format_trait_data$Species_all=="NA"]<- NA
+hclust_d_5$Species_all[hclust_d_5$Species_all=="NA"]<- NA
+
+#remove NA'S
+long_format_trait_data <- long_format_trait_data[!is.na(long_format_trait_data$Species_all),]
+hclust_d_5 <- hclust_d_5[!is.na(hclust_d_5$Species_all),]
+
+#merge
+quantitative_networks_Z_scores_with_traits_and_5_clusters <- merge(long_format_trait_data,hclust_d_5, by="Species_all", all.x  = T) #now data is ready for analysis
+head(quantitative_networks_Z_scores_with_traits_and_5_clusters)
+#remov 2 X'2 cloumn
+quantitative_networks_Z_scores_with_traits_and_5_clusters <- quantitative_networks_Z_scores_with_traits_and_5_clusters[,-c(3,4)]
+
+########################################################################################################################################################
+#5) SAVE DATA
+########################################################################################################################################################
+
+#SAVE 5 CLUSTERS
+write.csv(quantitative_networks_Z_scores_with_traits_and_5_clusters, "Data/Csv/quantitative_networks_Z_scores_with_traits_and_5_clusters_hclust.csv")
+
+########################################################################################################################################################
+########################################################################################################################################################
+########################################################################################################################################################
+
+
+
---
+++ Scripts/6_Analysis_Z_scores_Guilds_FunctionalG.R
@@ -0,0 +1,181 @@
+########################################################################################################################################################
+#SCRIPT FOR ANALYSIS (Z-SCORES~FUNCTIONAL GROUP*GUILD) ##HCLUST##
+
+#1)LOAD DATA 
+
+#2)PHYLOGENETIC DISTANCE OF THE SPECIES -Add them as covariable-
+
+#3)SAVE MODEL
+
+#4)PLOT OUTPUT NICELY
+########################################################################################################################################################
+
+#LOAD LIBRARIES
+library(ape) #for phylogenetic distance
+library(dplyr) #data processing
+library(rtrees) #for phylogenetic distance
+library(DHARMa)
+library(brms)
+library(cmdstanr)
+library(ggplot2)
+
+########################################################################################################################################################
+#1) READ DATA
+########################################################################################################################################################
+
+d_5 <- read.csv("Data/Csv/quantitative_networks_Z_scores_with_traits_and_5_clusters_hclust.csv", row.names = 1)
+
+########################################################################################################################################################
+#2) PHYLOGENETIC DISTANCE OF THE SPECIES
+########################################################################################################################################################
+
+#5 CLUSTERS
+#Prepare species, genus anD_5 family for calculating phylogenetic distance
+d_5$Family_all <- as.character(d_5$Family_all)
+d_5$Genus_all <- as.character(d_5$Genus_all)
+d_5$Species_all <- as.character(d_5$Species_all)
+
+#These species are not present for analysis in the matrix, remove or model cannot have phylo as covariable
+d_5$Species_all[d_5$Species_all=="Diospyros seychellarum"] <- NA
+d_5$Species_all[d_5$Species_all=="Memecylon eleagni"] <- NA
+d_5$Species_all[d_5$Species_all=="Ocotea laevigata"] <- NA
+d_5$Species_all[d_5$Species_all=="Soulamea terminaloides"] <- NA
+
+#Make these NA's as NA
+d_5$Species_all[d_5$Species_all=="NA"] <- NA
+d_5$Family_all[d_5$Family_all=="NA"] <- NA
+d_5$Genus_all[d_5$Genus_all=="NA"] <- NA
+#remove NA's
+d_5_1 <- d_5[!is.na(d_5$Family_all),]
+d_5_1 <- d_5[!is.na(d_5$Species_all),]
+d_5_1 <- d_5[!is.na(d_5$Genus_all),]
+
+#prepare dataframe to calculate tree
+phylo_5 <- as.data.frame(cbind(d_5_1$Family_all, d_5_1$Genus_all, d_5_1$Species_all))
+colnames(phylo_5) <-  c("family", "genus", "species")
+
+#Select unique cases
+phylo_5_1 <- phylo_5[!duplicated(phylo_5$species),]
+phylo_5_2 <- tibble(phylo_5_1)
+str(phylo_5_2)
+
+phylo_5_3 <- get_tree(sp_list = phylo_5_2, tree = tree_plant_otl, taxon = "plant")
+
+#Convert phylogenetic tree into matrix
+A_5 <- vcv.phylo(phylo_5_3)
+#Standardize to max value 1
+A_5 <- A_5/max(A_5)
+#Unify column names; remove underscore and remove asterik
+rownames(A_5) <- gsub("\\*", "", rownames(A_5))
+colnames(A_5) <- gsub("\\*", "", colnames(A_5))
+colnames(A_5) <- gsub("_", " ", colnames(A_5))
+rownames(A_5) <- gsub("_", " ", rownames(A_5))
+
+#Add phylo column to dataset
+d_5_1$phylo
+d_5_1$phylo <- d_5_1$Species_all
+str(d_5_1)
+
+d_5_1$Clusters <- as.character(d_5_1$Clusters)
+d_5_1$Clusters[d_5_1$Clusters=="1"] <- "A"
+d_5_1$Clusters[d_5_1$Clusters=="2"] <- "B"
+d_5_1$Clusters[d_5_1$Clusters=="3"] <- "C"
+d_5_1$Clusters[d_5_1$Clusters=="4"] <- "D"
+d_5_1$Clusters[d_5_1$Clusters=="5"] <- "E"
+
+d_5_1$Clusters <- as.factor(d_5_1$Clusters)
+levels(as.factor(d_5_1$Clusters))
+########################################################################################################################################################
+#3)ANALYSIS
+########################################################################################################################################################
+#STUDENT
+#5 clusters hclust
+m_5_clust_stu_hclust <- brm(Z_scores ~ guild*Clusters + (1|Id) + (1|gr(phylo, cov = A)),
+                            data = d_5_1, family  = student(),data2 = list(A = A_5), cores = 4,chains = 4, 
+                            sample_prior = TRUE, warmup = 500, iter = 1500,
+                            control = list(adapt_delta = 0.99)) 
+
+
+marginal_effects(m_5_clust_stu_hclust, effects = "Clusters:guild")
+pp_check(m_5_clust_stu_hclust) +xlim(-10,10)+ylim(0,3)
+pp_check(m_5_clust_stu_hclust, type='violin_grouped',group="Clusters")+ylim(-4,4)
+pp_check(m_5_clust_stu_hclust, type='violin_grouped',group="guild")+ylim(-4,4)
+
+#SAVE MODEL
+setwd("~/Dropbox/PhD/R") #DROPBOX, files too large for github
+saveRDS(m_5_clust_stu_hclust, "m_5_clust_stu_hclust.RDS")
+
+########################################################################################################################################################
+#)4)SAVE MODEL
+########################################################################################################################################################
+#SAVE MODEL
+setwd("~/Dropbox/PhD/R") #DROPBOX, files too large for github
+saveRDS(m_5_clust_stu_hclust, "m_5_clust_stu_hclust.RDS")
+
+########################################################################################################################################################
+#5)PLOT OUTPUT NICELY
+########################################################################################################################################################
+summary(m1)
+pp_check(m1) + xlim(-200,200)+ylim(0,0.2)
+ce <- conditional_effects(m_5_clust_stu_hclust, effects = "Clusters:guild",points=T) 
+levels(ce[[1]]$guild)
+#PLOT OUTPUT
+cat_plot(m1, pred = cyl, modx = fwd)
+
+colnames(ce[[1]])[3] <- "a"
+colnames(ce[[1]])[9] <- "Z_scores"
+
+
+ggplot(d_5_1, aes(x = Clusters, y = Z_scores, colour = as.factor(guild), group = 
+                  as.factor(guild))) +
+  geom_point(size = 1, position = position_dodge(width = 0.4), alpha=0.2) +
+  theme_bw()+ ylab("Standardize visits (Z-scores)") + xlab("Plant reproductive groups")+
+  geom_point(data=ce[[1]],group=as.factor(ce[[1]]$guild))
+
+
+
+
+ggplot(ce[[1]], aes(x = Clusters, y = Z_scores, colour = as.factor(guild), group = 
+                    as.factor(guild))) +
+  geom_point(size = 1, position = position_dodge(width = 0.4), alpha=1) +
+  theme_bw()+ ylab("Standardize visits (Z-scores)") + xlab("Plant reproductive groups")+
+  geom_point(data = d_5_1,aes(x = Clusters, y = Z_scores),size = 1, position = position_dodge(width = 0.4), alpha=0.15)+
+  geom_errorbar(data=ce[[1]],mapping=aes(x=Clusters, ymin=lower__, ymax=upper__,colour = as.factor(guild), group = 
+  as.factor(guild)), width=.4, position = position_dodge(width = 0.4))+ylim(-0.8,2.5)+
+  geom_violin(data = d_5_1,aes(x = Clusters, y = Z_scores,group = 
+                                 as.factor(guild)),size = 0.5, alpha=0.15, position = position_dodge(width = 0.1))
+  
+  
+  
+
+
+
+
+
+
+
+
+
+$››‹colnames(ce[[1]])
+
++
+  geom_errorbar(aes(ymax = val + SD, ymin = val - SD), position = 
+                  position_dodge(width = 0.2), width = 0.2) +
+  labs(y = "Mean # of explorations (+/- SD", colour = "pp")
+             
+
+
+ggplot(data=ce[[1]], aes(x = Clusters, y = Z_scores,color = ordered(guild))) +
+  geom_point(data = d_5_1,aes(x = Clusters, y = Z_scores,color = ordered(guild))) + 
+  scale_fill_brewer(palette = "Greys") +
+  scale_color_brewer(palette = "Set2") + theme_bw() +
+  geom_errorbar(data=p1.2.1[[1]],mapping=aes(x=autonomous_selfing_level, ymin=lower__, ymax=upper__), width=.1, color="black")+
+  geom_point(data=p1.2.1[[1]], mapping=aes(x=autonomous_selfing_level, y=estimate__), color="black") + ylab("Visits") + xlab("Selfing level")+
+  theme(legend.position = "none")
+
+
+
+########################################################################################################################################################
+########################################################################################################################################################
+########################################################################################################################################################
+
