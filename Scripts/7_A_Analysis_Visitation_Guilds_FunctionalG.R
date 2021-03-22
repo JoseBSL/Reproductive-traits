cb7bb159d880a24f137d88ac476c95e385887930
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.home>
2021-03-22 20:05:15 +0100

    run model at home takes too long

--- Scripts/7_A_Analysis_Visitation_Guilds_FunctionalG.R
+++ Scripts/7_A_Analysis_Visitation_Guilds_FunctionalG.R
@@ -26,19 +26,25 @@ library(ggplot2)
 ########################################################################################################################################################
 setwd("~/R_Projects/Reproductive Traits")
 d_5 <- read.csv("Data/Csv/quantitative_networks_Z_scores_with_traits_and_5_clusters_hclust_forest_data.csv", row.names = 1)
+levels(factor(d_5$guild))
+#I'm going to run the model just with the main poll. functional groups
+#exclude other insects level
+dat <- subset(d_5_1, guild!="Other_insects" & guild!="Lizards" & guild!="Birds")
+levels(factor(dat$guild))
+
+
 ########################################################################################################################################################
 #2) PHYLOGENETIC DISTANCE OF THE SPECIES
 ########################################################################################################################################################
 #5 CLUSTERS
 #Prepare species, genus anD_5 family for calculating phylogenetic distance
-d_5$Family_all <- as.character(d_5$Family_all)
-d_5$Genus_all <- as.character(d_5$Genus_all)
-d_5$Species_all <- as.character(d_5$Species_all)
+dat$Family_all  <- as.character(dat$Family_all)
+dat$Genus_all   <- as.character(dat$Genus_all)
+dat$Species_all <- as.character(dat$Species_all)
 
-d_5_1$Family_all[d_5_1$Plant_species=="Beta maritima"] <- "Amaranthaceae"
 
 #prepare dataframe to calculate tree
-phylo_5 <- as.data.frame(cbind(d_5_1$Family_all, d_5_1$Genus_all, d_5_1$Plant_species))
+phylo_5 <- as.data.frame(cbind(dat$Family_all, dat$Genus_all, dat$Plant_species))
 colnames(phylo_5) <-  c("family", "genus", "species")
 
 #Select unique cases
@@ -59,23 +65,22 @@ colnames(A_5) <- gsub("_", " ", colnames(A_5))
 rownames(A_5) <- gsub("_", " ", rownames(A_5))
 
 #Add phylo column to dataset
-d_5_1$phylo
-d_5_1$phylo <- d_5_1$Plant_species
-str(d_5_1)
+dat$phylo
+dat$phylo <- dat$Plant_species
+str(dat)
 
-d_5_1$Clusters <- as.character(d_5_1$Clusters)
-d_5_1$Clusters[d_5_1$Clusters=="1"] <- "A"
-d_5_1$Clusters[d_5_1$Clusters=="2"] <- "B"
-d_5_1$Clusters[d_5_1$Clusters=="3"] <- "C"
-d_5_1$Clusters[d_5_1$Clusters=="4"] <- "D"
-d_5_1$Clusters[d_5_1$Clusters=="5"] <- "E"
+dat$Clusters <- as.character(dat$Clusters)
+dat$Clusters[dat$Clusters=="1"] <- "A"
+dat$Clusters[dat$Clusters=="2"] <- "B"
+dat$Clusters[dat$Clusters=="3"] <- "C"
+dat$Clusters[dat$Clusters=="4"] <- "D"
+dat$Clusters[dat$Clusters=="5"] <- "E"
 
-d_5_1$Clusters <- as.factor(d_5_1$Clusters)
-levels(as.factor(d_5_1$Clusters))
-levels(factor(d_5_1$guild))
+dat$Clusters <- as.factor(dat$Clusters)
+levels(as.factor(dat$Clusters))
+levels(factor(dat$guild))
 
-#exclude other insects level
-d_5_1 <- subset(d_5_1, guild!="Other_insects")
+
 #save data
 saveRDS(d_5_1, "Data/Csv/data_model.RDS")
 ########################################################################################################################################################
@@ -84,7 +89,7 @@ saveRDS(d_5_1, "Data/Csv/data_model.RDS")
 
 #5 clusters hclust
 model_forest_data <- brm((Interaction-1) ~ guild*Clusters + (1|Id) + (1|gr(phylo, cov = A)),
-                            data = d_5_1, family  = zero_inflated_negbinomial(),data2 = list(A = A_5), cores = 4,chains = 4, 
+                            data = dat, family  = zero_inflated_negbinomial(),data2 = list(A = A_5), cores = 4,chains = 4, 
                             sample_prior = TRUE, warmup = 500, iter = 2000,
                             control = list(adapt_delta = 0.99)) 
 
@@ -103,7 +108,7 @@ ce <- conditional_effects(m_5_clust_zero_neg_hclust_famd, effects = "Clusters:gu
 ########################################################################################################################################################
 #SAVE MODEL 1
 setwd("~/Dropbox/PhD/R") #DROPBOX, files too large for github
-#saveRDS(model_forest_data, "model_forest_data.RDS")
+saveRDS(model_forest_data, "model_forest_data.RDS")
 ########################################################################################################################################################
 #4.2)PLOT OUTPUT VISITATION DATA
 ########################################################################################################################################################
