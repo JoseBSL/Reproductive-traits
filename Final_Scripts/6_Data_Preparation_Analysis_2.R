bb447caae8392f6b4474333d608818129f6cae5b
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.home>
2021-04-01 21:18:04 +0200

    Prepare last thing tomorrow

--- Final_Scripts/6_Data_Preparation_Analysis_2.R
+++ Final_Scripts/6_Data_Preparation_Analysis_2.R
@@ -1,3 +1,25 @@
 ########################################################################################################################################################
 #SCRIPT TO PREPARE DATA FOR ANALYSIS 2) VISITS/SPECIALIZATION ~ PRINCIPAL COMPONENTS
 ########################################################################################################################################################
+#LOAD LIBRARIES
+library(data.table) # operate with df
+library(bipartite) #calculate metrics
+library(dplyr) #data manipulation 
+library(stringr) #remove string
+########################################################################################################################################################
+#1) LOAD NETWORK DATA
+########################################################################################################################################################
+#Set working directory to read files
+setwd("~/R_Projects/Reproductive traits/Data/Data_networks_quantitative") 
+
+#read csv files
+temp <- list.files(pattern="*.csv")
+my.list <- list(for (i in 1:length(temp)) assign(temp[i], read.csv(temp[i])))
+my_files <- list.files(pattern = "\\.csv$")
+my_data <- lapply(my_files, function(i){read.csv(i,  row.names = 1)})
+# Add id to dataframe
+names(my_data) <- stringr::str_replace(my_files, pattern = ".csv", replacement = "")
+########################################################################################################################################################
+
+[KEEP WORKING FROM HERE]
+
