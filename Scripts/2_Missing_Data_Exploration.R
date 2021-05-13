aaff48faba2212d9c3198fd4fb91f0eff33cc363
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.home>
2021-05-13 19:47:35 +0200

    regression tree

--- Scripts/2_Missing_Data_Exploration.R
+++ Scripts/2_Missing_Data_Exploration.R
@@ -1,11 +1,19 @@
 ########################################################################################################################################################
-#SCRIPT TO CHECK PATTERNS OF MISSING VALUES
+#SCRIPT FOR DATA IMPUTATION
 ########################################################################################################################################################
 #1) READ TRAIT DATA
 
-#2) CLEAN DATA 
+#2) CLEAN DATA AND RENAME LEVELS
 
 #3) EXPLORE PATTERNS OF MISSING DATA
+
+#4) CALCULATE PHYLOGENETIC DISTANCE TO CORRECT WITH EIGENVALUES IN THE IMPUTATION
+
+#5) ICLUDE EIGENVALUES IN RAWDATA TO IMPROVE IMPUTATION OURPUT
+
+#6) IMPUTE DATA 
+
+#7) SAVE IMPUTED DATA 
 ########################################################################################################################################################
 #LOAD LIBRARIES
 library(readxl) #read excel file (trait data)
@@ -20,7 +28,6 @@ library(ape)
 library(PVR) #calculate eigen vectors and add them as a column in the imputation, seems to improve output
 library(visdat) #VISUALIZE MISSING DATA
 library(naniar)
-library(VIM)
 
 ########################################################################################################################################################
 #1) READ TRAIT DATA
@@ -47,15 +54,133 @@ trait_filtered_2 <- trait_filtered_1[!duplicated(trait_filtered_1$Species_all),]
 str(trait_filtered_2)
 
 #Select columns to work with
-t <- trait_filtered_2[c("Species_geonet","Family_all","Genus_all","Species_all","Breeding_system","Compatibility_system",
+t <- trait_filtered_2[c("Species_geonet","Order_all","Family_all","Genus_all","Species_all","Breeding_system","Compatibility_system",
                         "Autonomous_selfing_level","Autonomous_selfing_level_fruit_set", "Flower_morphology", "Flower_symmetry", 
                         "Flowers_per_plant", "Floral_unit_width", "Corolla_diameter_mean", "Corolla_length_mean","Style_length", "Ovule_number", 
-                        "life_form", "lifespan","Plant_height_mean_m","Nectar_presence_absence","Nectar_ul")]
+                        "life_form", "lifespan","Plant_height_mean_m","Nectar_presence_absence","Nectar_ul","Nectar_mg","Nectar_concentration",
+                        "Pollen_per_flower")]
 
 
-####################################################################################################
-#2) CLEAN DATA. I REPEAT THE SAME PROCEDURE OF THE DATA IMPUTATION TO GET THE SAME NUMBER OF SPECIES
-####################################################################################################
+########################################################################################################################################################
+#####
+#B) RENAME LEVELS FOR IMPUTATION
+#####
+#Breeding_system| For the purpose of this study we are not going to consider other complex breeding systems and just focus on 
+#Hermaphroditism, dioecy and monoecy, the closest levels are grouped within each category
+t$Breeding_system <- as.character(t$Breeding_system)
+t$Breeding_system[t$Breeding_system=="androdioecious"] <- "dioecious"
+t$Breeding_system[t$Breeding_system=="androgynodioecious"] <- "dioecious"
+t$Breeding_system[t$Breeding_system=="andromonoecious"] <- "monoecious"
+t$Breeding_system[t$Breeding_system=="gynodioecious"] <- "dioecious"
+t$Breeding_system[t$Breeding_system=="gynoecious"] <- "monoecious"
+t$Breeding_system[t$Breeding_system=="gynomonoecious"] <- "monoecious"
+t$Breeding_system[t$Breeding_system=="gynomonoecious"] <- "monoecious"
+t$Breeding_system[t$Breeding_system=="monoecious_dioecious"] <- "monoecious"
+t$Breeding_system[t$Breeding_system=="polygamo-dioecious"] <- "dioecious"
+t$Breeding_system[t$Breeding_system=="protandrous"] <- "hermaphrodite"
+t$Breeding_system[t$Breeding_system=="protogynous"] <- "hermaphrodite"
+t$Breeding_system[t$Breeding_system=="subdioecious"] <- "dioecious"
+t$Breeding_system[t$Breeding_system=="submonoecious"] <- "monoecious"
+#Change to capital letters| There are already levels with lower case
+t$Breeding_system[t$Breeding_system=="monoecious"] <- "Monoecious"
+t$Breeding_system[t$Breeding_system=="dioecious"] <- "Dioecious"
+t$Breeding_system[t$Breeding_system=="hermaphrodite"] <- "Hermaphrodite"
+levels(as.factor(t$Breeding_system))
+t$Breeding_system <- as.factor(t$Breeding_system)
+
+#Now 3 levels
+#Flower_morphology
+t$Flower_morphology <- as.character(t$Flower_morphology)
+t$Flower_morphology[t$Flower_morphology=="bowl"] <- "open"
+t$Flower_morphology[t$Flower_morphology=="dish"] <- "open"
+t$Flower_morphology[t$Flower_morphology=="exposed"] <- "open"
+t$Flower_morphology[t$Flower_morphology=="spadix"] <- "spike"
+t$Flower_morphology[t$Flower_morphology=="open"] <- "Open"
+t$Flower_morphology[t$Flower_morphology=="brush"] <- "Brush"
+t$Flower_morphology[t$Flower_morphology=="campanulate"] <- "Campanulate"
+t$Flower_morphology[t$Flower_morphology=="capitulum"] <- "Capitulum"
+t$Flower_morphology[t$Flower_morphology=="funnelform"] <- "Funnelform"
+t$Flower_morphology[t$Flower_morphology=="papilionaceous"] <- "Papilionaceous"
+t$Flower_morphology[t$Flower_morphology=="spike"] <- "Spike"
+t$Flower_morphology[t$Flower_morphology=="tube"] <- "Tube"
+levels(as.factor(t$Flower_morphology))
+t$Flower_morphology <- as.factor(t$Flower_morphology)
+
+#Life span
+t$lifespan <- as.character(t$lifespan )
+t$lifespan[t$lifespan=="annual"] <- "short_lived"
+t$lifespan[t$lifespan=="biennial"] <- "short_lived"
+t$lifespan[t$lifespan=="short_lived"] <- "Short lived"
+t$lifespan[t$lifespan=="perennial"] <- "Perennial"
+levels(as.factor(t$lifespan))
+t$lifespan <- as.factor(t$lifespan )
+
+########################################################################################################################################################
+#3) EXPLORE PATTERNS OF MISSING DATA
+########################################################################################################################################################
+#Explore patterns of missing data
+missing_data <- unlist(lapply(t, function(x) sum(is.na(x))))/nrow(t)
+sort(missing_data[missing_data >= 0], decreasing=T)
+#nectar values all above 50% except presence/absence
+#then, just one cololumn with 68% missing data, then one with 35% and then 20% and 10%. The rest under 5%.
+#it is recommended to remove columns over 50% but we are particulary interested in having numeric levels of quantitative selfing 
+#we are going to work just with presence/absence of nectar
+#we will perform further analysis just with a subset with the quantitative values o nectar (very interesting data and diffciult to obtain)
+
+
+#We convert cualitative data to quantitative to reduce missing data on this column and hence conduct the imputation 
+#use ifelse base r does not work with these NA'S Even with work around. base r does not like the na option of read excel
+t$Autonomous_selfing_level_fruit_set <- ifelse(t$Autonomous_selfing_level %in% c("high") & is.na(t$Autonomous_selfing_level_fruit_set), 88, t$Autonomous_selfing_level_fruit_set)
+t$Autonomous_selfing_level_fruit_set <- ifelse(t$Autonomous_selfing_level %in% c("medium") & is.na(t$Autonomous_selfing_level_fruit_set), 50.5, t$Autonomous_selfing_level_fruit_set)
+t$Autonomous_selfing_level_fruit_set <- ifelse(t$Autonomous_selfing_level %in% c("low") & is.na(t$Autonomous_selfing_level_fruit_set), 13, t$Autonomous_selfing_level_fruit_set)
+t$Autonomous_selfing_level_fruit_set <- ifelse(t$Autonomous_selfing_level %in% c("none") & is.na(t$Autonomous_selfing_level_fruit_set), 0, t$Autonomous_selfing_level_fruit_set)
+
+#t$Nectar_ul adding it here just to see how many cells 
+#Now we just select presence/absence of nectar
+#select columns of interest
+t <- t[c("Species_geonet","Order_all","Family_all","Genus_all","Species_all","Breeding_system","Compatibility_system",
+         "Autonomous_selfing_level","Autonomous_selfing_level_fruit_set", "Flower_morphology", "Flower_symmetry", 
+         "Flowers_per_plant", "Floral_unit_width", "Corolla_diameter_mean", "Corolla_length_mean","Style_length", "Ovule_number", 
+         "life_form", "lifespan","Plant_height_mean_m","Nectar_presence_absence","Nectar_ul","Nectar_mg","Nectar_concentration",
+         "Pollen_per_flower")]
+
+
+
+
+
+#check missing data now
+missing_data <- unlist(lapply(t, function(x) sum(is.na(x))))/nrow(t)*100
+sort(missing_data[missing_data >= 0], decreasing=T)
+
+
+missing_data <- unlist(lapply(t, function(x) nrow(t)-sum(is.na(x))))
+sum(missing_data)
+
+#Number of missing values on the datset
+#vis_miss(t[,c(6:20)])
+#9.9% percentage of missing values]
+
+#Checking number of filled cells/ just selecting variables used in the manuscript; nectar ul should be accounted too and it has been considered but
+#not added to run this code
+
+########################################################################################################################################################
+#4) CALCULATE PHYLOGENETIC DISTANCE TO CORRECT WITH EIGENVALUES IN THE IMPUTATION
+########################################################################################################################################################
+#Convert to factor 
+cols <- c("Order_all", "Family_all", "Genus_all", "Species_all", "Compatibility_system", "Autonomous_selfing_level", "Flower_morphology",
+          "Flower_symmetry", "life_form", "lifespan","Nectar_presence_absence")
+t[cols] <- lapply(t[cols], factor)  ## as.factor() could also be used
+str(t)
+
+
+#Impute data considering phylogenetic distance of species
+#it improves quality of imputation
+#https://doi.org/10.1111/2041-210X.12232 see ref for method comparisons with and without imputation
+
+#Multiple imputation process with phylogeny --> Missforest
+#https://doi.org/10.1111/2041-210X.12232
+#althought it does not have an option we are going to add it as a column
+
 #Before data imputation I need to calculate the phylo of the species
 #remove not found species that do not run with get tree
 cols.num <- c("Family_all","Genus_all","Species_all")
@@ -85,30 +210,31 @@ t <- t[!is.na(t$Family_all),]
 t <- t[!is.na(t$Species_all),]
 t <- t[!is.na(t$Genus_all),]
 
-nrow(t)
 
-#Select just columns with traits
 t <- t[c("Breeding_system","Compatibility_system",
-                        "Autonomous_selfing_level","Autonomous_selfing_level_fruit_set", "Flower_morphology", "Flower_symmetry", 
-                        "Flowers_per_plant", "Floral_unit_width", "Corolla_diameter_mean", "Corolla_length_mean","Style_length", "Ovule_number", 
-                        "life_form", "lifespan","Plant_height_mean_m","Nectar_presence_absence","Nectar_ul")]
+         "Autonomous_selfing_level","Autonomous_selfing_level_fruit_set", "Flower_morphology", "Flower_symmetry", 
+         "Flowers_per_plant", "Floral_unit_width", "Corolla_diameter_mean", "Corolla_length_mean","Style_length", "Ovule_number", 
+         "life_form", "lifespan","Plant_height_mean_m","Nectar_presence_absence","Nectar_ul","Nectar_mg","Nectar_concentration",
+         "Pollen_per_flower")]
 
 
 #################################################
 #3)SUMMARY OF THE DATA|ALL COLS CONSIDERED!
 #################################################
 #Number of filled cells
-nrow(t) * 16 - sum(is.na(t))#[1] 20737
+nrow(t) * 20 - sum(is.na(t))#[1] 23969
 #Dimension of the dataset
-nrow(t) * 16#[1] 24096
+nrow(t) * 20#[1] 30120
 #Number of missing values
-sum(is.na(t))#[1] 3359
+sum(is.na(t))#[1] 6151
 #Percentage of filled cells
-sum(!is.na(t))/(sum(is.na(t))+sum(!is.na(t)))*100#[1] 86.87993
+sum(!is.na(t))/(sum(is.na(t))+sum(!is.na(t)))*100#[1] 79.57835
+
 #Percentage of missing values
-sum(is.na(t))/(sum(is.na(t))+sum(!is.na(t)))*100#[1] 13.12007
+sum(is.na(t))/(sum(is.na(t))+sum(!is.na(t)))*100#[1] 20.42165
 
 
+(nrow(t) * 20 - sum(is.na(t))) / (nrow(t) * 20) *100
 
 #Function to specify number of decimals
 specify_decimal <- function(x, k) trimws(format(round(x, k), nsmall=k))
@@ -118,10 +244,12 @@ no_missing <- specify_decimal(sum(!is.na(t))/(sum(is.na(t))+sum(!is.na(t)))*100,
 missing <- specify_decimal(sum(is.na(t))/(sum(is.na(t))+sum(!is.na(t)))*100,2)
 
 
-colnames(t) <- c("Breeding system","Compatibility system",
-         "Autonomous selfing qualitative","Autonomous selfing quantitative", "Flower shape", "Flower symmetry", 
-         "Flower number per plant", "Floral display width", "Flower width", "Flower length","Style length", "Ovule number", 
-         "Life form", "Lifespan","Plant height","Nectar qualitative","Nectar quantitative")
+t <- t[c("Breeding_system","Compatibility_system",
+         "Autonomous_selfing_level","Autonomous_selfing_level_fruit_set", "Flower_morphology", "Flower_symmetry", 
+         "Flowers_per_plant", "Floral_unit_width", "Corolla_diameter_mean", "Corolla_length_mean","Style_length", "Ovule_number", 
+         "life_form", "lifespan","Plant_height_mean_m","Nectar_presence_absence","Nectar_ul","Nectar_mg","Nectar_concentration",
+         "Pollen_per_flower")]
+
 
 
 library(naniar)
---
+++ Scripts/Regression_Tree_All.R
@@ -0,0 +1,239 @@
+########################################################################################################################################################
+########################################################################################################################################################
+########################################################################################################################################################
+##REGRESSION TREE FOR ALL THE SPECIES
+########################################################################################################################################################
+########################################################################################################################################################
+########################################################################################################################################################
+
+#HERE I TRY TO SELECT THE MAIN EXPLANATORY TRAITS FOR EACH TRAIT 
+
+#LOAD LIBRARIES
+library(data.table) # operate with df
+library(bipartite) #calculate metrics
+library(dplyr) #data manipulation 
+library(stringr) #remove string
+library(brms)
+library(projpred)
+library(dplyr)
+library(ggplot2)
+library(bayesplot)
+library(ape)
+library(rtrees)
+library(cowplot)
+library(emmeans)
+library(tidybayes)
+library(multcomp)
+########################################################################################################################################################
+#1) LOAD NETWORK DATA
+########################################################################################################################################################
+#Set working directory to read files
+setwd("~/R_Projects/Reproductive traits/Data/Data_networks_quantitative") 
+
+#read csv files
+temp <- list.files(pattern="*.csv")
+my.list <- list(for (i in 1:length(temp)) assign(temp[i], read.csv(temp[i])))
+my_files <- list.files(pattern = "\\.csv$")
+my_data <- lapply(my_files, function(i){read.csv(i,  row.names = 1)})
+# Add id to dataframe
+names(my_data) <- stringr::str_replace(my_files, pattern = ".csv", replacement = "")
+
+########################################################################################################################################################
+#2) CALCULATE METRICS 
+########################################################################################################################################################
+
+#Function to sum visits per row and convert NA'S to 0's in the case there is any
+rs_NA <- function(x){
+  z <- as.data.frame(x)
+  z[is.na(z)] <- 0  #convert NA'S to 0's
+  z <- as.data.frame(rowSums(z)) #sum rows
+  z <- setDT(z, keep.rownames = TRUE)[] 
+  colnames(z) <- c("Species","Visits")
+  return(z)
+}
+
+
+#Calculate all metrics at once with bipartite
+met <- function(x){
+  Visits_Sum <- rs_NA(x) #previous function to sum rows 
+  #degree
+  degree <- specieslevel(x, index="degree", level="lower")
+  #normalise degree
+  n_degree <- specieslevel(x, index="normalised degree", level="lower")
+  #specialization
+  d_Plant <- specieslevel(x, index="d", level="lower")   # specialization from BLUTHGEN 2006
+  #closeness
+  closeness <- specieslevel(x, index="closeness", level="lower")
+  #betweenness
+  betweenness <- specieslevel(x, index="betweenness", level="lower")
+  #combine metrics in a unique data frame
+  metrics <- cbind(Visits_Sum,n_degree,d_Plant, closeness, betweenness, degree) #degree, n_degree, d, closeness, betweenness)
+  return(metrics)
+}
+
+#workaround to remove singletones
+i <- NULL
+data <- NULL
+metrics_list <- list()
+for (i in names(my_data)){
+  metrics_list[[i]] <- my_data[[i]][apply(my_data[[i]][,-1], 1, function(x) !all(x<2)),]
+}
+
+#calculate network metrics with for loop
+i <- NULL
+data <- NULL
+metrics_list_1 <- list()
+for (i in names(metrics_list)){
+  metrics_list_1[[i]] <- met(metrics_list[[i]])
+}
+
+#add id as a row name
+all_list <- lapply(seq_along(metrics_list_1),function(x) cbind(metrics_list_1[[x]], 
+                                                               unique.id=str_replace(my_files[x], pattern = ".csv", replacement = "")))
+
+#Now merge all the data frames 
+all_df <- bind_rows(all_list)
+
+#check
+#all_df_unique_sp <- all_df[!duplicated(all_df$Species),]
+
+#remove species that are not until species level
+#ALL THE SPECIES WITH SP. ARE DELETD
+all_df$Species <-  sub('sp.1$', 'sp.', all_df$Species)
+all_df$Species <- sub('sp.2$', 'sp.', all_df$Species)
+all_df$Species <- sub('sp$', 'sp.', all_df$Species)
+all_df$Species <- sub("sp.$", "DELETE", all_df$Species)
+
+all_df <- all_df[- grep("DELETE",  all_df$Species),] #remove species with "DELETE"
+
+colnames(all_df)[1] <- "Species_all"
+
+########################################################################################################################################################
+########################################################################################################################################################
+########################################################################################################################################################
+
+#SET NORMAL WORKING DIRECTORY
+setwd("~/R_Projects/Reproductive traits") 
+
+dat <- read.csv("Data/Csv/all_species_imputed_trait_data_forest_data.csv", row.names = "X")
+
+#The correct species names are the row names, convert to 1st col to merge
+#convert row to col
+setDT(dat, keep.rownames = TRUE)[]
+#set a colname
+colnames(dat)[1] <- "Plants"
+#merge
+datitos <- merge(all_df, dat, by.x =  "Species_all", by.y= "Plants")
+
+
+#New colname with log of visits
+
+datitos_1 <- datitos
+#FIRST QUALITATIVE VARIABLES
+#Check levels before running models and perform cross validation
+levels(factor(datitos_1$Breeding_system)) # ok
+levels(factor(datitos_1$Compatibility_system)) #Fix
+datitos_1$Compatibility_system[datitos_1$Compatibility_system=="dioecious"] <- "Unisexual flowers"
+datitos_1$Compatibility_system[datitos_1$Compatibility_system=="monoecious"] <- "Unisexual flowers"
+datitos_1$Compatibility_system[datitos_1$Compatibility_system=="self_incompatible"] <- "Self incompatible"
+datitos_1$Compatibility_system[datitos_1$Compatibility_system=="self_compatible"] <- "Self compatible"
+datitos_1$Compatibility_system[datitos_1$Compatibility_system=="partially_self_compatible"] <- "Partially self compatible"
+#Check again
+levels(factor(datitos_1$Compatibility_system)) #NOW OK
+levels(factor(datitos_1$Autonomous_selfing_level)) 
+datitos_1$Autonomous_selfing_level[datitos_1$Autonomous_selfing_level=="high"] <- "High-Medium"
+datitos_1$Autonomous_selfing_level[datitos_1$Autonomous_selfing_level=="medium"] <- "High-Medium"
+datitos_1$Autonomous_selfing_level[datitos_1$Autonomous_selfing_level=="low"] <- "Low-None"
+datitos_1$Autonomous_selfing_level[datitos_1$Autonomous_selfing_level=="none"] <- "Low-None"
+levels(factor(datitos_1$Autonomous_selfing_level)) #Ok 
+levels(factor(datitos_1$Flower_morphology)) 
+datitos_1$Flower_morphology[datitos_1$Flower_morphology=="Funnelform"] <- "Campanulate"
+datitos_1$Flower_morphology[datitos_1$Flower_morphology=="Spike"] <- "Brush"
+levels(factor(datitos_1$Flower_morphology)) #OK
+levels(factor(datitos_1$Flower_symmetry)) 
+datitos_1$Flower_symmetry[datitos_1$Flower_symmetry=="actinomorphic"] <- "Actinomorphic"
+datitos_1$Flower_symmetry[datitos_1$Flower_symmetry=="zygomorphic"] <- "Zygomorphic"
+levels(factor(datitos_1$Flower_symmetry)) #OK
+levels(factor(datitos_1$life_form)) 
+datitos_1$life_form[datitos_1$life_form=="herb"] <- "Herb"
+datitos_1$life_form[datitos_1$life_form=="shrub"] <- "Shrub"
+datitos_1$life_form[datitos_1$life_form=="tree"] <- "Tree"
+datitos_1$life_form[datitos_1$life_form=="vine"] <- "Shrub"
+levels(factor(datitos_1$life_form)) #OK
+levels(factor(datitos_1$lifespan)) #OK
+levels(factor(datitos_1$Nectar_presence_absence))
+datitos_1$Nectar_presence_absence[datitos_1$Nectar_presence_absence=="yes"] <- "Yes"
+datitos_1$Nectar_presence_absence[datitos_1$Nectar_presence_absence=="no"] <- "No"
+levels(factor(datitos_1$Nectar_presence_absence)) #OK
+
+
+datitos_2 <- as.data.frame(datitos_1)
+
+
+
+#Now quantitative variables
+########################################################################################################################################################
+#Visits quantitative 
+########################################################################################################################################################
+df <- datitos_2[c("Visits", "Autonomous_selfing_level_fruit_set","Flowers_per_plant","Corolla_diameter_mean", "Style_length","Ovule_number",
+                  "Plant_height_mean_m")]
+
+
+tree <- rpart(Visits~., data=df, cp=0.001)
+printcp(tree)
+plotcp(tree)
+tree <- rpart(Visits~., data=df, cp=0.0077368)
+rpart.plot(tree, box.palette="GnOr")
+
+df <- df[c("Visits", "Autonomous_selfing_level_fruit_set","Flowers_per_plant","Corolla_diameter_mean","Style_length","Ovule_number",
+           "Plant_height_mean_m","Nectar_ul","Nectar_mg","Nectar_concentration", "Pollen_per_flower")]
+
+
+tree <- rpart(Visits~., data=df, cp=0.0086964) #8 divisions
+rpart.plot(tree, box.palette="GnOr")
+printcp(tree)
+plotcp(tree)
+
+
+#Alternative method with poissot and lower error
+#Root node error is quite high
+
+tree <- rpart(Visits~., data=df, cp=0.016487,method = "poisson")
+printcp(tree)
+rpart.plot(tree, box.palette="GnOr", extra=100)
+
+
+########################################################################################################################################################
+#Visits qualitative
+########################################################################################################################################################
+
+#here I try an alternative way that is less elegant but more honest due to its shows the % of each grouping variable on the box
+
+df <- datitos_2[c("Visits", "Autonomous_selfing_level_fruit_set","Flowers_per_plant","Corolla_diameter_mean","Style_length","Ovule_number",
+                  "Plant_height_mean_m")]
+
+df$v_q
+
+df$v_q[df$Visits<quantile(df$Visits, 0.33)] <- "Low" 
+df$v_q[df$Visits>=quantile(df$Visits, 0.33)] <- "Medium" 
+df$v_q[df$Visits>quantile(df$Visits, 0.66)] <- "High" 
+
+df$v_q <- factor(df$v_q, levels=c("Low", "Medium", "High"))
+
+
+df <- df[c("v_q", "Autonomous_selfing_level_fruit_set","Flowers_per_plant","Corolla_diameter_mean","Style_length","Ovule_number",
+           "Plant_height_mean_m")]
+
+
+tree <- rpart(v_q~., data=df, cp=0.01, method= "class") #6node divisons
+rpart.plot(tree, box.palette="GnOr")
+
+printcp(tree)
+plotcp(tree)
+rpart.plot(tree, yesno = TRUE)
+
+tree <- rpart(v_q~., data=df, cp=0.010893, method= "class") #6node divisons
+rpart.plot(tree, box.palette="GnOr")
+
+
+
