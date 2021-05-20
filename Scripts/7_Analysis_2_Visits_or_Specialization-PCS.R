503dc4311bb98837c71467be194f0d6507b5d8a1
Jose Barragán Sánchez-Lanuza <lanuza@Joses-MBP.home>
2021-05-20 13:15:05 +0200

    EDITS

--- Scripts/7_Analysis_2_Visits_or_Specialization-PCS.R
+++ Scripts/7_Analysis_2_Visits_or_Specialization-PCS.R
@@ -59,7 +59,7 @@ data_analysis_2$System[grepl("elberling_sweeden_1999", data_analysis_2$System)] 
 ########################################################################################################################################################
 Visits_PCs <- brm((Visits-1) ~ PC1 + PC2 + PC3  +(1|System/Id),
                   data = data_analysis_2, family  = zero_inflated_negbinomial(), cores = 4,chains = 4, 
-                  sample_prior = TRUE, warmup = 500, iter = 2000,
+                  sample_prior = TRUE, warmup = 1000, iter = 3000,
                   control = list(adapt_delta = 0.99)) 
 
 #looks ok
@@ -101,19 +101,19 @@ Visits_PC3 <- ggplot(ce_3[[1]], aes(x = -PC3, y = (estimate__+1))) + geom_point(
 
 cowplot::plot_grid(Visits_PC1,Visits_PC2,Visits_PC3, ncol=3)
 ########################################################################################################################################################
-
+# MODEL SPECIALIZATION ~ PRINCIPAL COMPONENTS
+########################################################################################################################################################
 data_analysis_2$d <- ifelse(data_analysis_2$d > 1, 1, data_analysis_2$d)
 
 Specialization_PCs <- brm(d ~ PC1 + PC2 + PC3 + (1|System/Id),
                           data = data_analysis_2, family  = zero_one_inflated_beta(), cores = 4,chains = 4, 
-                          sample_prior = TRUE, warmup = 500, iter = 2000,
+                          sample_prior = TRUE, warmup = 1000, iter = 3000,
                           control = list(adapt_delta = 0.99)) 
 
 
 summary(Specialization_PCs)
 
 performance::r2(Specialization_PCs)
-
 
 
 #Save output
@@ -150,60 +150,14 @@ cowplot::plot_grid(Specialization_PC1, Specialization_PC2, Specialization_PC3)
 
 cowplot::plot_grid(Visits_PC1,Visits_PC2,Visits_PC3,Specialization_PC1, Specialization_PC2, Specialization_PC3, ncol=3)
 
-########################################################################################################################################################
-#degree
-########################################################################################################################################################
-
-hist(data_analysis_2$degree)
-
-degree <- brm(degree ~ PC1 + PC2 + PC3 + (1|System/Id),
-                             data = data_analysis_2, family  = zero_inflated_negbinomial(), cores = 4,chains = 4, 
-                             sample_prior = TRUE, warmup = 500, iter = 2000,
-                             control = list(adapt_delta = 0.99)) 
-
-pp_check(degree)
-
-summary(degree)
-
-conditional_effects(degree, effects = "PC1",points=T) 
-conditional_effects(degree, effects = "PC2",points=T) 
-conditional_effects(degree, effects = "PC3",points=T) 
-
-#Save output
-setwd("~/Dropbox/PhD/R/Chapter_2") #DROPBOX, files too large for github
-saveRDS(degree, "results_analysis_2_degree.rds") 
-#read
-degree <- readRDS("results_analysis_2_degree.rds") #load model
-
-
-ce_1 <- conditional_effects(degree, effects = "PC1",points=T) 
-
-degree_PC1 <- ggplot(ce_1[[1]], aes(x = -PC1, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC1, y = degree),
- size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2)  +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("degree")
-
-ce_2 <- conditional_effects(degree, effects = "PC2",points=T) 
-
-degree_PC2 <- ggplot(ce_2[[1]], aes(x = -PC2, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC2, y = degree),
-  size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2) +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("degree")
-
-ce_3 <- conditional_effects(degree, effects = "PC3",points=T) 
-
-degree_PC3 <- ggplot(ce_3[[1]], aes(x = -PC3, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC3, y = degree),
- size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2) +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("degree")
-
-degree_PC1 + degree_PC2 + degree_PC3
-
 
 ########################################################################################################################################################
-#normalised.degree
+# MODEL NORMALIZED DEGREE ~ PRINCIPAL COMPONENTS
 ########################################################################################################################################################
 
 normalised.degree_PCs <- brm(normalised.degree ~ PC1 + PC2 + PC3 + (1|System/Id),
                              data = data_analysis_2, family  = weibull(), cores = 4,chains = 4, 
-                             sample_prior = TRUE, warmup = 500, iter = 3000,
+                             sample_prior = TRUE, warmup = 1000, iter = 3000,
                              control = list(adapt_delta = 0.99)) 
 
 
@@ -247,228 +201,11 @@ normalised.degree_PC3 <- ggplot(ce_3[[1]], aes(x = -PC3, y = (estimate__))) + ge
 normalised.degree_PC1 + normalised.degree_PC2 + normalised.degree_PC3
 
 
-cowplot::plot_grid(Visits_PC1,Visits_PC2,Visits_PC3,Specialization_PC1, Specialization_PC2, Specialization_PC3, 
-                   normalised.degree_PC1, normalised.degree_PC2, normalised.degree_PC3, ncol=3)
-
-########################################################################################################################################################
-##betweenness
-########################################################################################################################################################
-
-data_analysis_2$betweenness <- ifelse(data_analysis_2$betweenness > 1, 1, data_analysis_2$betweenness)
-
-betweenness <- brm(betweenness ~ PC1 + PC2 + PC3 + (1|Id),
-              data = data_analysis_2, family  = zero_one_inflated_beta(), cores = 4,chains = 4, 
-              sample_prior = TRUE, warmup = 500, iter = 2000,
-              control = list(adapt_delta = 0.99)) 
-
-summary(betweenness)
-
-pp_check(betweenness)
-
-
-ce_1 <- conditional_effects(betweenness, effects = "PC1",points=T) 
-
-
-#Save output
-setwd("~/Dropbox/PhD/R/Chapter_2") #DROPBOX, files too large for github
-saveRDS(betweenness, "results_analysis_2_betweenness.rds") 
-#read
-betweenness <- readRDS("results_analysis_2_betweenness.rds") #load model
-
-
-betweenness_PC1 <- ggplot(ce_1[[1]], aes(x = -PC1, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC1, y = betweenness),
-  size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2)  +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("betweenness")
-
-ce_2 <- conditional_effects(betweenness, effects = "PC2",points=T) 
-
-betweenness_PC2 <- ggplot(ce_2[[1]], aes(x = -PC2, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC2, y = betweenness),
-  size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2) +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("betweenness")
-
-ce_3 <- conditional_effects(betweenness, effects = "PC3",points=T) 
-
-betweenness_PC3 <- ggplot(ce_3[[1]], aes(x = -PC3, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC3, y = betweenness),
-   size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2) +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("betweenness")
-
-betweenness_PC1 + betweenness_PC2 + betweenness_PC3
-
-########################################################################################################################################################
-##weighted betweenness
-########################################################################################################################################################
-
-data_analysis_2$weighted.betweenness <- ifelse(data_analysis_2$weighted.betweenness > 1, 1, data_analysis_2$weighted.betweenness)
-
-weighted.betweenness <- brm(weighted.betweenness ~ PC1 + PC2 + PC3 + (1|Id),
-                   data = data_analysis_2, family  = zero_one_inflated_beta(), cores = 4,chains = 4, 
-                   sample_prior = TRUE, warmup = 500, iter = 2000,
-                   control = list(adapt_delta = 0.99)) 
-
-summary(weighted.betweenness)
-
-pp_check(weighted.betweenness)
-
-#Save output
-setwd("~/Dropbox/PhD/R/Chapter_2") #DROPBOX, files too large for github
-saveRDS(weighted.betweenness, "results_analysis_2_weighted.betweenness.rds") 
-#read
-weighted.betweenness <- readRDS("results_analysis_2_weighted.betweenness.rds") #load model
-
-
-ce_1 <- conditional_effects(weighted.betweenness, effects = "PC1",points=T) 
-
-weighted.betweenness_PC1 <- ggplot(ce_1[[1]], aes(x = -PC1, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC1, y = weighted.betweenness),
-  size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2)  +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("weighted.betweenness")
-
-ce_2 <- conditional_effects(weighted.betweenness, effects = "PC2",points=T) 
-
-weighted.betweenness_PC2 <- ggplot(ce_2[[1]], aes(x = -PC2, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC2, y = weighted.betweenness),
-   size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2) +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("weighted.betweenness")
-
-ce_3 <- conditional_effects(weighted.betweenness, effects = "PC3",points=T) 
-
-weighted.betweenness_PC3 <- ggplot(ce_3[[1]], aes(x = -PC3, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC3, y = weighted.betweenness),
-   size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2) +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("weighted.betweenness")
-
-weighted.betweenness_PC1 + weighted.betweenness_PC2 + weighted.betweenness_PC3
-########################################################################################################################################################
-##PDI
-########################################################################################################################################################
-
-data_analysis_2$PDI <- ifelse(data_analysis_2$PDI > 1, 1, data_analysis_2$PDI)
-
-PDI <- brm(PDI ~ PC1 + PC2 + PC3 + (1|Id),
-                   data = data_analysis_2, family  = zero_one_inflated_beta(), cores = 4,chains = 4, 
-                   sample_prior = TRUE, warmup = 500, iter = 2000,
-                   control = list(adapt_delta = 0.99)) 
-
-pp_check(PDI)
-
-summary(PDI)
-
-#Save output
-setwd("~/Dropbox/PhD/R/Chapter_2") #DROPBOX, files too large for github
-saveRDS(PDI, "results_analysis_2_PDI.rds") 
-#read
-PDI <- readRDS("results_analysis_2_PDI.rds") #load model
-
-
-ce_1 <- conditional_effects(PDI, effects = "PC1",points=T) 
-
-PDI_PC1 <- ggplot(ce_1[[1]], aes(x = -PC1, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC1, y = PDI),
-   size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2)  +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("PDI")
-
-ce_2 <- conditional_effects(PDI, effects = "PC2",points=T) 
-
-PDI_PC2 <- ggplot(ce_2[[1]], aes(x = -PC2, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC2, y = PDI),
-  size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2) +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("PDI")
-
-ce_3 <- conditional_effects(PDI, effects = "PC3",points=T) 
-
-PDI_PC3 <- ggplot(ce_3[[1]], aes(x = -PC3, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC3, y = PDI),
-  size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2) +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("PDI")
-
-PDI_PC1 + PDI_PC2 + PDI_PC3
-
-########################################################################################################################################################
-##CLOSENESS
-########################################################################################################################################################
-
-closeness <- brm(closeness ~ PC1 + PC2 + PC3 + (1|Id),
-           data = data_analysis_2, family  = zero_one_inflated_beta(), cores = 4,chains = 4, 
-           sample_prior = TRUE, warmup = 500, iter = 2000,
-           control = list(adapt_delta = 0.99)) 
-
-pp_check(closeness)
-
-summary(closeness)
-
-
-ce_1 <- conditional_effects(closeness, effects = "PC1",points=T) 
-
-#Save output
-setwd("~/Dropbox/PhD/R/Chapter_2") #DROPBOX, files too large for github
-saveRDS(PDI, "results_analysis_2_closeness.rds") 
-#read
-closeness <- readRDS("results_analysis_2_closeness.rds") #load model
-
-
-ce_1 <- conditional_effects(closeness, effects = "PC1",points=T) 
-
-closeness_PC1 <- ggplot(ce_1[[1]], aes(x = -PC1, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC1, y = closeness),
-  size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2)  +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("closeness")
-
-ce_2 <- conditional_effects(closeness, effects = "PC2",points=T) 
-
-closeness_PC2 <- ggplot(ce_2[[1]], aes(x = -PC2, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC2, y = closeness),
-  size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2) +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("closeness")
-
-ce_3 <- conditional_effects(closeness, effects = "PC3",points=T) 
-
-closeness_PC3 <- ggplot(ce_3[[1]], aes(x = -PC3, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC3, y = closeness),
-  size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2) +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("closeness")
-
-
-closeness_PC1 + closeness_PC2 + closeness_PC3
-
-########################################################################################################################################################
-##WEIGHTED CLOSENESS
-########################################################################################################################################################
-
-#weighted.closeness <- brm(weighted.closeness ~ PC1 + PC2 + PC3 + (1|Id),
-                # data = data_analysis_2, family  = zero_one_inflated_beta(), cores = 4,chains = 4, 
-                # sample_prior = TRUE, warmup = 500, iter = 2000,
-                # control = list(adapt_delta = 0.99)) 
-
-pp_check(weighted.closeness)
-
-summary(weighted.closeness)
-
-#Save output
-setwd("~/Dropbox/PhD/R/Chapter_2") #DROPBOX, files too large for github
-saveRDS(PDI, "results_analysis_2_weighted.closeness.rds") 
-#read
-weighted.closeness <- readRDS("results_analysis_2_weighted.closeness.rds") #load model
-
-
-ce_1 <- conditional_effects(weighted.closeness, effects = "PC1",points=T) 
-
-weighted.closeness_PC1 <- ggplot(ce_1[[1]], aes(x = -PC1, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC1, y = weighted.closeness),
- size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2)  +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("weighted.closeness")
-
-ce_2 <- conditional_effects(weighted.closeness, effects = "PC2",points=T) 
-
-weighted.closeness_PC2 <- ggplot(ce_2[[1]], aes(x = -PC2, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC2, y = weighted.closeness),
-  size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2) +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("weighted.closeness")
-
-ce_3 <- conditional_effects(weighted.closeness, effects = "PC3",points=T) 
-
-weighted.closeness_PC3 <- ggplot(ce_3[[1]], aes(x = -PC3, y = (estimate__))) + geom_point(data = data_analysis_2,aes(x = -PC3, y = weighted.closeness),
-  size = 1, alpha=0.9) + geom_line(colour="darkblue",size=1.2) +theme_ms()+
-  geom_ribbon(aes(ymin=(lower__), ymax=(upper__)), linetype=2, alpha=0.1,fill="darkblue") + ylab("weighted.closeness")
-
-
-weighted.closeness_PC1 + weighted.closeness_PC2 + weighted.closeness_PC3
-
 ########################################################################################################################################################
 #PLOTALL#PLOTALL#PLOTALL#PLOTALL#PLOTALL#PLOTALL#PLOTALL#PLOTALL#PLOTALL#PLOTALL#PLOTALL#PLOTALL#PLOTALL#PLOTALL#PLOTALL#PLOTALL#PLOTALL#PLOTALL#PLOTALL
 ########################################################################################################################################################
 
-Visits_PC1 + Visits_PC2 + Visits_PC3 + degree_PC1 + degree_PC2 + degree_PC3+ normalised.degree_PC1 + normalised.degree_PC2 + normalised.degree_PC3 +
-  Specialization_PC1 + Specialization_PC2+ Specialization_PC3+  PDI_PC1 + PDI_PC2 + PDI_PC3+   plot_layout(ncol = 3)
+Visits_PC1 + Visits_PC2 + Visits_PC3 + normalised.degree_PC1 + normalised.degree_PC2 + normalised.degree_PC3 +
+  Specialization_PC1 + Specialization_PC2+ Specialization_PC3  + plot_layout(ncol = 3)
   
-  
-  
-#NECTAR
+
