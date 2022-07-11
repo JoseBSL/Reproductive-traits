c8c0b29a14b7cae0ecde9348144c720afec59d5f
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.comcd>
2022-07-11 18:30:24 +0200

    edits

---
+++ Google_drive.R
@@ -0,0 +1,21 @@
+
+
+#Upload files to google drive
+#install packages
+remotes::install_github("claudiozandonella/trackdown",
+                        build_vignettes = TRUE, force=T)
+
+install.packages("googledrive")
+
+#set google doc
+googledrive::drive_auth()
+
+#load libraries
+library(googledrive)
+library(trackdown)
+
+#upload file
+upload_file(
+  file = "Manuscript/Lanuza_et_al_2022.Rmd", 
+  gfile = "Untitled"
+)
